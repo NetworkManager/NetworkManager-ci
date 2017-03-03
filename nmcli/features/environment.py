bfa32fa2c7ea72ef7965912c0886f89df38b2283
Vladimir Benes <vbenes@redhat.com>
2017-03-03 19:18:31 +0100

    env: add hostnamectl to environemnt status log
    Change-Id: I931f2e7bd8741191e60c6e38055bfd8f3d2f76a2

--- nmcli/features/environment.py
+++ nmcli/features/environment.py
@@ -50,8 +50,9 @@ def dump_status(context, when):
             call(cmd, shell=True, stdout=context.log)
     else:
         for cmd in ['ip addr', 'ip -4 route', 'ip -6 route',
-            'nmcli g', 'nmcli c', 'nmcli d', 'nmcli -f IN-USE,SSID,CHAN,SIGNAL,SECURITY d w', 'nmcli con show testeth0',\
-            'sysctl -a|grep ra |grep ipv6 |grep "all\|default\|eth\|test"']:
+            'nmcli g', 'nmcli c', 'nmcli d', 'nmcli -f IN-USE,SSID,CHAN,SIGNAL,SECURITY d w', 'hostnamectl']:
+            #'nmcli con show testeth0',\
+            #'sysctl -a|grep ra |grep ipv6 |grep "all\|default\|eth\|test"']:
             context.log.write("--- %s ---\n" % cmd)
             context.log.flush()
             call(cmd, shell=True, stdout=context.log)
