9a1c4f4f229e74e17e6784e2b1950756567a4071
Vladimir Benes <vbenes@redhat.com>
2017-07-20 14:08:22 +0200

    beah: make robust workarounder for beaker harness weirdness
    Change-Id: I6613aee7efe03746b05f23ef72726582670be8f8

--- run/rh-beaker/sanitize_beah.sh
+++ run/rh-beaker/sanitize_beah.sh
@@ -1,18 +1,30 @@
 #! /bin/bash
 set -x
+sudo sysctl net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl net.ipv6.conf.all.disable_ipv6=0
 
-nmcli dev disconnect eth0
 nmcli con del testeth0
+ip addr flush dev eth0
+
 sleep 1
+
 nmcli connection add type ethernet ifname eth0 con-name testeth0
 nmcli connection modify testeth0 ipv6.may-fail no
-nmcli con up id testeth0
+nmcli con up testeth0
+nmcli con down testeth0
+
+sleep 5
+
+systemctl restart NetworkManager
+sleep 30
+
 ip a s eth0
 ip -6 r
-sleep 60
 
-systemctl condreload beah-beaker-backend.service
+systemctl restart beah-beaker-backend.service
 
-sleep 60
+sleep 180
+
+rhts-report-result $TEST "PASS" "/dev/null"
 
 exit 0
