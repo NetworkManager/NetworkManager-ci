NetworkManager/NetworkManager-ci

commit 98fef041b412ee803ecae651b9bc9fb9c0bb25ec

IPv6: add test for preservation of cached routes
https://bugzilla.redhat.com/show_bug.cgi?id=1470930

nmcli/features/ipv6.feature
@@ -1086,3 +1086,16 @@ Feature: nmcli: ipv6
      And "1.2.3.4" is visible with command "ip a s testX"
      And "2001::2" is visible with command "ip a s testX"
      And "tentative" is visible with command "ip a s testX" for full "10" seconds
+
+    @rhbz1470930
+    @ver+=1.8.0
+    @ethernet @teardown_testveth @netcat
+    @ipv6_preserve_cached_routes
+    Scenario: NM - ipv6 - preserve cached routes
+    * Prepare simulated test "testX" device for IPv6 PMTU discovery
+    * Add a new connection of type "ethernet" and options "ifname testX con-name ethernet0 autoconnect no"
+    * Execute "nmcli con modify ethernet0 ipv4.method disabled ipv6.method auto"
+    * Execute "ip l set testX up"
+    * Bring "up" connection "ethernet0"
+    * Execute "dd if=/dev/zero bs=1M count=10 | nc fd02::2 8080"
+    Then "mtu 1400" is visible with command "ip route get fd02::2" for full "40" seconds

nmcli/features/environment.py
@@ -527,6 +527,13 @@ def before_scenario(context, scenario):
             call("nmcli connection up testeth8", shell=True)
             call("nmcli connection delete eth8", shell=True)
 
+        if 'netcat' in scenario.tags:
+            print ("---------------------------")
+            print ("installing netcat")
+            wait_for_testeth0()
+            if not os.path.isfile('/usr/bin/nc'):
+                call('sudo yum -y install nmap-ncat', shell=True)
+
         if 'scapy' in scenario.tags:
             print ("---------------------------")
             print ("installing scapy and tcpdump")

nmcli/features/steps/steps.py
@@ -1399,6 +1399,57 @@ def prepare_simdev(context, device):
     context.testvethns.append("%s2_ns" % device)
 
 
+@step(u'Prepare simulated test "{device}" device for IPv6 PMTU discovery')
+def prepare_simdev(context, device):
+    if not hasattr(context, 'testvethns'):
+        os.system('''echo 'ENV{ID_NET_DRIVER}=="veth", ENV{INTERFACE}=="test*", ENV{NM_UNMANAGED}="0"' >/etc/udev/rules.d/88-lr.rules''')
+        command_code(context, "udevadm control --reload-rules")
+        command_code(context, "udevadm settle")
+        command_code(context, "sleep 1")
+    #         +-------testX_ns--------+ +--testX2_ns--+
+    # testX <-|-> testXp     testX2 <-|-|-> testX2p   |
+    #         |  fd01::1     fd02::1  | |   fd02::2   |
+    # mtu 1500|   1500        1400    | |    1500     |
+    #         +-----------------------+ +-------------+
+    command_code(context, "ip netns add {device}_ns".format(device=device))
+    command_code(context, "ip netns add {device}2_ns".format(device=device))
+    command_code(context, "ip link add {device} type veth peer name {device}p".format(device=device))
+    command_code(context, "ip link add {device}2 type veth peer name {device}2p".format(device=device))
+    command_code(context, "ip link set {device}p netns {device}_ns".format(device=device))
+    command_code(context, "ip link set {device}2 netns {device}_ns".format(device=device))
+    command_code(context, "ip link set {device}2p netns {device}2_ns".format(device=device))
+    # Bring up devices
+    command_code(context, "ip link set {device} up".format(device=device))
+    command_code(context, "ip netns exec {device}_ns ip link set {device}p up".format(device=device))
+    command_code(context, "ip netns exec {device}_ns ip link set {device}2 up".format(device=device))
+    command_code(context, "ip netns exec {device}2_ns ip link set {device}2p up".format(device=device))
+    # Set addresses
+    command_code(context, "ip netns exec {device}_ns ip addr add dev {device}p fd01::1/64".format(device=device))
+    command_code(context, "ip netns exec {device}_ns ip addr add dev {device}2 fd02::1/64".format(device=device))
+    command_code(context, "ip netns exec {device}2_ns ip addr add dev {device}2p fd02::2/64".format(device=device))
+    # Set MTU
+    command_code(context, "ip netns exec {device}_ns ip link set {device}p mtu 1500".format(device=device))
+    command_code(context, "ip netns exec {device}_ns ip link set {device}2 mtu 1400".format(device=device))
+    command_code(context, "ip netns exec {device}2_ns ip link set {device}2p mtu 1500".format(device=device))
+    # Set up router (testX_ns)
+    command_code(context, "ip netns exec {device}_ns sh -c 'echo 1 > /proc/sys/net/ipv6/conf/all/forwarding'".format(device=device))
+    command_code(context, "ip netns exec {device}_ns dnsmasq \
+                                         --no-resolv \
+                                         --pid-file=/tmp/{device}_ns.pid \
+                                         --bind-interfaces -i {device}p \
+                                         --enable-ra \
+                                         --dhcp-range=::1,::400,constructor:{device}p,ra-only,64,15s".format(device=device))
+    # Add route
+    command_code(context, "ip netns exec {device}2_ns ip route add fd01::/64 via fd02::1 dev {device}2p".format(device=device))
+    # Run netcat server to receive some data
+    Popen("ip netns exec {device}2_ns nc -6 -l -p 8080 > /dev/null".format(device=device) , shell=True)
+
+    if not hasattr(context, 'testvethns'):
+        context.testvethns = []
+    context.testvethns.append("%s_ns" % device)
+    context.testvethns.append("%s2_ns" % device)
+
+
 @step(u'Prepare simulated veth device "{device}" wihout carrier')
 def prepare_simdev_no_carrier(context, device):
     ipv4 = "192.168.99"

commit cb1949af9202dc393f9c9cbecde9756e958934ce

ipv6: add explicit route to server in @ipv6_preserve_cached_routes
Otherwise fd02::2 will be contacted on the wrong interface if there is
an existing default IPv6 route with lower metric.

Fixes: 98fef041b412ee803ecae651b9bc9fb9c0bb25ec

nmcli/features/ipv6.feature
@@ -1095,6 +1095,7 @@ Feature: nmcli: ipv6
     * Prepare simulated test "testX" device for IPv6 PMTU discovery
     * Add a new connection of type "ethernet" and options "ifname testX con-name ethernet0 autoconnect no"
     * Execute "nmcli con modify ethernet0 ipv4.method disabled ipv6.method auto"
+    * Execute "nmcli con modify ethernet0 ipv6.routes 'fd02::/64 fd01::1'"
     * Execute "ip l set testX up"
     * Bring "up" connection "ethernet0"
     * Execute "dd if=/dev/zero bs=1M count=10 | nc fd02::2 8080"