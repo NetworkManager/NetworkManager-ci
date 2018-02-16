46ea9fda82761d2e057c20e2df0b7fc64175b5cb
Vladimir Benes <vbenes@redhat.com>
2018-02-16 10:30:22 +0100

    add NetworkManager --print-config output to dump too

--- nmcli/features/environment.py
+++ nmcli/features/environment.py
@@ -52,7 +52,8 @@ def dump_status(context, when):
             call(cmd, shell=True, stdout=context.log)
     else:
         for cmd in ['ip addr', 'ip -4 route', 'ip -6 route',
-            'nmcli g', 'nmcli c', 'nmcli d', 'nmcli -f IN-USE,SSID,CHAN,SIGNAL,SECURITY d w', 'hostnamectl']:
+            'nmcli g', 'nmcli c', 'nmcli d', 'nmcli -f IN-USE,SSID,CHAN,SIGNAL,SECURITY d w',
+            'hostnamectl', 'NetworkManager --print-config']:
             #'nmcli con show testeth0',\
             #'sysctl -a|grep ra |grep ipv6 |grep "all\|default\|eth\|test"']:
             context.log.write("--- %s ---\n" % cmd)
