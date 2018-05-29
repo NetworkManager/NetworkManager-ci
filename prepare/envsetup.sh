ecdf0c47dd3bee626a2cf072b420a2a53f5de581
Vladimir Benes <vbenes@redhat.com>
2018-05-29 13:46:57 +0200

    envsetup: add skip-broken to install ntpd

--- prepare/envsetup.sh
+++ prepare/envsetup.sh
@@ -13,7 +13,7 @@ local_setup_configure_nm_eth () {
     echo "networkmanager" | passwd test --stdin
 
     #adding ntp and syncing time
-    yum -y install dnsmasq ntp tcpdump NetworkManager-libreswan wireshark bridge-utils
+    yum -y install dnsmasq ntp tcpdump NetworkManager-libreswan wireshark bridge-utils --skip-broken
 
     service ntpd restart
 
