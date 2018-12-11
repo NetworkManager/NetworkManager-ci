408575229f65d89f1da77b7ad039d81c54fc0ebb
Filip Pokryvka <41154041+fpokryvk@users.noreply.github.com>
2018-12-11 12:45:27 +0100

    add "libnm_async_activation_cancellable_no_crash" test (#270)
    use reproducer from Gris Ge 
    https://bugzilla.redhat.com/show_bug.cgi?id=1642625#c11 
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1643085
    
    FIX: nmcli_monitor_assertion_con_up_down uses eth2 instead of eth8

---
+++ tmp/repro_1643085.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# all credits to Gris Ge https://bugzilla.redhat.com/show_bug.cgi?id=1642625#c11
+from subprocess import check_call
+import sys
+
+import gi
+gi.require_version('NM', '1.0')
+from gi.repository import Gio, GLib, NM
+
+
+
+nmclient = NM.Client.new()
+
+conn_name = sys.argv[1]
+conn_ifname = sys.argv[2]
+
+conn = nmclient.get_connection_by_id(conn_name)
+
+if not conn:
+    check_call([ 'nmcli', 'c', 'add', 'type', 'ethernet', 'ifname', conn_ifname,
+                 'connection.id', conn_name])
+    nmclient = NM.Client.new()
+    conn = nmclient.get_connection_by_id('con_general')
+    if not conn:
+        print("Failed to find newly created connection")
+        exit()
+
+cancellable = Gio.Cancellable.new()
+mainloop = GLib.MainLoop()
+
+user_data = cancellable
+
+def _active_connection_callback(src_object, result, user_data):
+    user_data.cancel()
+    mainloop.quit()
+
+nmclient.activate_connection_async(conn, None, None, cancellable,
+                                   _active_connection_callback, user_data)
+
+mainloop.run()
