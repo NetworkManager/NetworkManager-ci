c982da14a9f3b23cb2e5c9e3dbb1d6588e3b1ecc
Filip Pokryvka <41154041+fpokryvk@users.noreply.github.com>
2019-01-22 16:48:03 +0100

    add 'captive_portal_detection' test (#279)
    Add test, that NM recognizes portal connections.
    Test changes DNS to point to localhost and starts local HTTP server, and 
    according to response of HTTP server, NM should detect whether 
    connection is portal or not.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1658217

---
+++ prepare/captive_portal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function setup () {
+    # override DNS static.redhat.com to localhost
+    echo "0.0.0.0 static.redhat.com" >> /etc/hosts
+    # prepare directory for http server
+    mkdir -p /tmp/python_http/test/
+    cd /tmp/python_http
+    echo -n "OK" > test/rhel-networkmanager.txt
+    # run python http server (sharing working directory "/tmp/python_http/")
+    if which python3 &> /dev/null; then
+        #python3
+        python3 -m http.server 80 &
+        echo $! > /tmp/python_http.pid
+    else
+        #python2
+        python2 -m SimpleHTTPServer 80 &
+        echo $! > /tmp/python_http.pid
+    fi
+}
+
+
+function teardown () {
+    # remove dns override
+    sed '/^0\.0\.0\.0 static\.redhat\.com/d' -i /etc/hosts
+    # stop http server and clean up
+    kill $(cat /tmp/python_http.pid)
+    rm -rf /tmp/python_http/
+}
+
+
+if [ "x$1" != "xteardown" ]; then
+    setup
+else
+    teardown
+fi
