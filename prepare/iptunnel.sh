18fc1cf4770afe7d4e5784d488fb715cdd23af92
Filip Pokryvka <fpokryvk@redhat.com>
2019-02-06 13:51:44 +0100

    iptunnel: move setup and teardown from environmemt to prepare script

---
+++ prepare/iptunnel.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+function teardown() {
+    ip l del ipip1
+    ip l del veth0
+    ip netns del iptunnel
+    nmcli connection delete iptunnel-veth ipip1 gre1
+}
+
+function setup() {
+    # prepare namespace and veth pair
+    ip netns add iptunnel
+    ip link add veth0 type veth peer name veth1
+    ip link set veth1 netns iptunnel
+    ip -n iptunnel link set veth1 up
+    ip -n iptunnel address add dev veth1 172.25.16.2/24
+    # prepare tunnels in namespace - ipip
+    ip -n iptunnel tunnel add ipip2 mode ipip remote 172.25.16.1 local 172.25.16.2 dev veth1
+    ip -n iptunnel link set ipip2 up
+    ip -n iptunnel address add dev ipip2 172.25.30.2/24
+    # prepare tunnels in namespace - gre
+    ip -n iptunnel tunnel add gre2 mode gre remote 172.25.16.1 local 172.25.16.2 dev veth1
+    ip -n iptunnel link set gre2 up
+    ip -n iptunnel address add dev gre2 172.25.31.2/24
+    ip -n iptunnel -6 address add dev gre2  fe80:dead::beef/64
+    # prepare nmcli
+    nmcli connection add type ethernet ifname veth0 con-name iptunnel-veth ip4 172.25.16.1/24
+    nmcli connection up iptunnel-veth
+}
+
+if [ "$1" == "teardown" ] ; then
+    teardown
+else
+    setup
+fi
