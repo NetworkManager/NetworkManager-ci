0dfa7a09a3c3e67e8ff1d1d7f41a7adb5c2083f9
Vladimír Beneš <benesv@email.cz>
2019-02-22 14:01:29 +0100

    Improve prereqs installation methods (#294)
    * envsetup: improved package installations
     * install multiple times if rpm is missing
     * move installations to distro specific functions
    
    * env:  add env setup for Fedora
     * ethernet: add logging to 8021x
     * skip_in_fedora tag and usage of it
     * env: install new behave-1.6 with HTML formatter for python 3.7

--- nmcli/features/environment.py
+++ nmcli/features/environment.py
@@ -491,6 +491,13 @@ def before_scenario(context, scenario):
                 print ("---------------------------")
                 sys.exit(77)
 
+        if 'skip_in_fedora' in scenario.tags:
+            print ("---------------------------")
+            print ("Skipping in Fedoras")
+            if call("grep -q Fedora /etc/redhat-release", shell=True) == 0:
+                print ("---------------------------")
+                sys.exit(77)
+
         if 'mock' in scenario.tags:
             print ("---------------------------")
             print ("installing dbus-x11, pip, and python-dbusmock")
@@ -748,6 +755,7 @@ def before_scenario(context, scenario):
             print("----------------------------")
             print("iptunnel setup")
             call('sh prepare/iptunnel.sh', shell=True)
+
 
         # if 'macsec' in scenario.tags:
         #     print("---------------------------")
