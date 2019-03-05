5a8c14c583539f706b220b5d5391160a0014c8c4
Filip Pokryvka <fpokryvk@redhat.com>
2019-03-05 08:10:03 +0100

    Increase stability of step 'Check bond "{bond}" link state is "{state}" '
    Step is sometimes failing, increased number of tries and lowered timeout
    between checks (to increase time performance)
    
    This is because step checks bond state in procfs, and it is kernel
    dependent, when bond state changes there.

--- nmcli/features/steps/bond_bridge_team.py
+++ nmcli/features/steps/bond_bridge_team.py
@@ -87,13 +87,13 @@ def check_bond_link_state(context, bond, state):
     ret = False
     if os.system('ls /proc/net/bonding/%s' %bond) != 0 and state == "down":
         return
-    i = 4
+    i = 40
     while i > 0:
         child = pexpect.spawn('cat /proc/net/bonding/%s' % (bond), encoding='utf-8')
         if child.expect(["MII Status: %s" %  state, pexpect.EOF]) == 0:
             return
         else:
-            sleep(1)
+            sleep(0.2)
             i-=1
     assert child.expect(["MII Status: %s" %  state, pexpect.EOF]) == 0, "%s is not in %s link state" % (bond, state)
 
