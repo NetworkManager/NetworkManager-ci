9765a71aced6662fcdfde79854a5ac5d7184436f
Pavlin Georgiev <pgeorgie@redhat.com>
2019-03-26 13:07:24 +0100

    Create a function for testing modems on Acroname USB hub

--- nmcli/runtest.sh
+++ nmcli/runtest.sh
@@ -6,6 +6,64 @@ logger -t $0 "Running test $1"
 export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 DIR=$(pwd)
 
+function test_modems_usb_hub() {
+    local MODEM_COUNT=${1:?"Error: number of modems is not specified."}
+    # Number of modems that are plugged into Acroname USB hub.
+    local PORT_COUNT=${2:?"Error: number of USB ports on hub is not specified."}
+    local RC=0
+
+    if [ -z "$NMTEST" ]; then
+        echo "Error: variable \"NMTEST\" is not initialized." >&2
+        return 1
+    fi
+
+    if [ -z "$DIR" ]; then
+        echo "Error: variable \"DIR\" for working directory is not specified." >&2
+        return 1
+    fi
+
+    touch /tmp/usb_hub
+    echo "USB_HUB" > /tmp/report_$NMTEST.html
+    for m in $(seq 0 1 $((MODEM_COUNT-1)) ); do
+        for p in $(seq 0 1 $((PORT_COUNT-1)) ); do
+            $DIR/tmp/usb_hub/acroname.py --port $p --disable
+        done
+        sleep 1
+
+        $DIR/tmp/usb_hub/acroname.py --port $m --enable
+
+        # wait for device to appear in NM
+        TIMER=60
+        while [ $TIMER -gt 0 ]; do
+            if nmcli d |grep -q gsm; then
+                break
+            else
+                sleep 1
+                ((TIMER--))
+            fi
+        done
+
+        # Test a single modem connected to the USB hub.
+        # Test 1 of 2.
+        behave $DIR/nmcli/features -t gsm_create_default_connection -k -f html -o /tmp/report_1.html -f plain
+        if [ $? -eq 1 ]; then
+            RC=1
+        fi
+
+        cat /tmp/report_1.html >> /tmp/report_$NMTEST.html
+
+        # Test 2 of 2.
+        behave $DIR/nmcli/features -t gsm_disconnect -k -f html -o /tmp/report_2.html -f plain
+        if [ $? -eq 1 ]; then
+            RC=1
+        fi
+
+        # Concatenate HTML reports from 2 test into 1.
+        cat /tmp/report_2.html >> /tmp/report_$NMTEST.html
+    done
+}  # test_modems_usb_hub
+
+
 . $DIR/prepare/envsetup.sh
 setup_configure_environment "$1"
 
@@ -40,45 +98,8 @@ elif [ $vc -eq 0 ]; then
     else
         # check if we have gsm_hub use this
         if [ $1 == 'gsm_hub' ];then
-            MODEM_COUNT=3
-            PORT_COUNT=8
-            rc=0
-            touch /tmp/usb_hub
-            echo "USB_HUB" > /tmp/report_$NMTEST.html
-            # Number of modems that are plugged into Acroname USB hub.
-            for m in $(seq 0 1 $((MODEM_COUNT-1)) ); do
-                for p in $(seq 0 1 $((PORT_COUNT-1)) ); do
-                    $DIR/tmp/usb_hub/acroname.py --port $p --disable
-                done
-                sleep 1
-                
-                $DIR/tmp/usb_hub/acroname.py --port $m --enable
-
-                # wait for device to appear in NM
-                TIMER=60                
-                while [ $TIMER -gt 0 ]; do
-                    if nmcli d |grep -q gsm; then
-                        break
-                    else
-                        sleep 1
-                        ((TIMER--))
-                    fi
-                done
-                
-                behave $DIR/nmcli/features -t gsm_create_default_connection -k -f html -o /tmp/report_1.html -f plain
-                if [ $? -eq 1 ]; then
-                    rc=1
-                fi
-                
-                cat /tmp/report_1.html >> /tmp/report_$NMTEST.html
-
-                behave $DIR/nmcli/features -t gsm_disconnect -k -f html -o /tmp/report_2.html -f plain
-                if [ $? -eq 1 ]; then
-                    rc=1
-                fi
-                
-                cat /tmp/report_2.html >> /tmp/report_$NMTEST.html
-            done
+            # Test 3 modems on USB hub with 8 ports.
+            test_modems_usb_hub 3 8; rc=$?
 
         # if we do not have tag or gsm_hub
         else
@@ -87,13 +108,14 @@ elif [ $vc -eq 0 ]; then
     fi
 fi
 
-RESULT="FAIL"
+
 if [ $rc -eq 0 ]; then
     RESULT="PASS"
-fi
-if [ $rc -eq 77 ]; then
+elif [ $rc -eq 77 ]; then
     RESULT="SKIP"
     rc=0
+else
+    RESULT="FAIL"
 fi
 
 rstrnt-report-result -o "/tmp/report_$NMTEST.html" $NMTEST $RESULT
