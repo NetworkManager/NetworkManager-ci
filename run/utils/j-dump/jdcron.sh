df2f04fc78e88c8a65d4332e7d11d8b032855ce8
Francesco Giudici <fgiudici@redhat.com>
2019-08-27 11:12:23 +0200

    j-dump: jdcron.sh: add generation of html index file

--- run/utils/j-dump/jdcron.sh
+++ run/utils/j-dump/jdcron.sh
@@ -12,19 +12,7 @@
 JDUMP_BIN="$PWD"/j-dump.py
 OUTPUT_DIR="/tmp/j_dump/"
 LOG_FILE="logger.txt"
-
-log() {
-	echo "$*" >> "$LOG_FILE"
-}
-
-USER="$1"
-PASSWORD="$2"
-
-mkdir -p "$OUTPUT_DIR"
-cd "$OUTPUT_DIR"
-log "-----------------------------------------------------------------"
-log `date`
-log "-----------------------------------------------------------------"
+HTML_INDEX_FILE="index.html"
 
 # Put in BASE_URL the base url of your NM jenkins projects and in PROJECTS
 # the project names (this allows to collect separate logs from multiple projects)
@@ -32,11 +20,60 @@ log "-----------------------------------------------------------------"
 # results for 3 projects (3 different branches).
 BASE_URL="https://ci.centos.org/view/NetworkManager/job/NetworkManager-"
 PROJECTS="master nm-1-10 nm-1-8"
+PROJECT_LABEL="CentosCI"
+
+USER="$1"
+PASSWORD="$2"
+
+[ -n "$USER" -a -n "$PASSWORD" ] && JDUMP_OPTIONS="--user $USER --password $PASSWORD"
+
+log() {
+	echo "$*" >> "$LOG_FILE"
+}
+
+index_html_heading() {
+	echo -e '<!DOCTYPE html>\n' \
+	        '<html>\n' \
+	        '  <head>\n' \
+	        '    <style> * { font-family:arial, sans-serif; } </style>\n' \
+	        '  </head>\n' \
+	        '  <body>\n' \
+	        '    <h1>NetworManager CI results</h1>\n' \
+	        '    <ul>' \
+	> $HTML_INDEX_FILE
+}
+
+index_html_add_entry() {
+	ref="$1_index.html"
+	name="$2"
+
+	echo "      <li><a href=${ref}>${name}</a></li>" >> $HTML_INDEX_FILE
+}
+
+index_html_trailing() {
+	echo -e '    </ul>\n' \
+	        '  </body>\n' \
+	        '</html>' \
+	>> $HTML_INDEX_FILE
+}
+
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR"
+log "-----------------------------------------------------------------"
+log `date`
+log "-----------------------------------------------------------------"
+
+index_html_heading
 
 for proj in $PROJECTS
 do
-	$JDUMP_BIN --name CentosCI-"$proj" "$BASE_URL""$proj" >> "$LOG_FILE" 2>&1
+	PROJECT_FULL_NAME="${PROJECT_LABEL}-${proj}"
+
+	$JDUMP_BIN $JDUMP_OPTIONS --name "$PROJECT_FULL_NAME" "$BASE_URL""$proj" >> "$LOG_FILE" 2>&1
+	index_html_add_entry "$PROJECT_FULL_NAME" "${proj}"
 done
+
+index_html_trailing
 
 [ "$?" = "0" ] && log "*** Success ***"
 log "@@-------------------------------------------------------------@@"
