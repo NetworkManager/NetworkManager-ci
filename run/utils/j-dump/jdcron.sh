f59c940c7be881bffba6652de484788a90a96f65
Francesco Giudici <fgiudici@redhat.com>
2019-09-24 12:16:26 +0200

    j-dump: fixup jdcron.sh script
    Align it after the rework to j-dump

--- run/utils/j-dump/jdcron.sh
+++ run/utils/j-dump/jdcron.sh
@@ -2,25 +2,28 @@
 
 ###
 # This script runs in batch j-dump sessions to collect result summaries from
-# multiple jenkins projects.
-# It will produce as output two html files per project:
-# - ${PROJECT_NAME}_index.html
-# - ${PROJECT_NAME}_failures.html
+# multiple jenkins jobs.
+# It will produce as output two html files per job:
+# - ${job}_builds.html
+# - ${job}_failures.html
 ###
 
 # Change the JDUMP_BIN to the path of j-dump.py script if required.
-JDUMP_BIN="$PWD"/j-dump.py
+JDUMP_BIN="$PWD/j-dump.py"
 OUTPUT_DIR="/tmp/j_dump/"
 LOG_FILE="logger.txt"
 HTML_INDEX_FILE="index.html"
 
-# Put in BASE_URL the base url of your NM jenkins projects and in PROJECTS
-# the project names (this allows to collect separate logs from multiple projects)
+# Put in JENKINS_URL the base url of your jenkins server.
+# Put in JOBS the job list you want to process (logs will be collected separately
+# for each job).
+# If your jobs have a common header, you can drop it from the jobs listed in JOBS
+# and put it in JOB_HEADER.
 # Here a working example for the public NetworkManager CI on CentosCI, collecting
-# results for 3 projects (3 different branches).
-BASE_URL="https://ci.centos.org/view/NetworkManager/job/NetworkManager-"
-PROJECTS="master nm-1-10 nm-1-8"
-PROJECT_LABEL="CentosCI"
+# results for 3 jobs (matching 3 different branches on gitlab).
+JENKINS_URL="https://ci.centos.org"
+JOB_HEADER="NetworkManager-"
+JOBS="master nm-1-20 nm-1-18"
 
 USER="$1"
 PASSWORD="$2"
@@ -44,7 +47,7 @@ index_html_heading() {
 }
 
 index_html_add_entry() {
-	ref="$1_index.html"
+	ref="$1_builds.html"
 	name="$2"
 
 	echo "      <li><a href=${ref}>${name}</a></li>" >> $HTML_INDEX_FILE
@@ -65,12 +68,13 @@ log "-----------------------------------------------------------------"
 
 index_html_heading
 
-for proj in $PROJECTS
+for job in $JOBS
 do
-	PROJECT_FULL_NAME="${PROJECT_LABEL}-${proj}"
+	JOB_FULL_NAME="${JOB_HEADER}${job}"
+	[ -n "$JOB_HEADER" ] && JDUMP_JOB_NAME="--name $job" || unset JDUMP_JOB_NAME
 
-	$JDUMP_BIN $JDUMP_OPTIONS --name "$PROJECT_FULL_NAME" "$BASE_URL""$proj" >> "$LOG_FILE" 2>&1
-	index_html_add_entry "$PROJECT_FULL_NAME" "${proj}"
+	$JDUMP_BIN $JDUMP_OPTIONS $JDUMP_JOB_NAME "$JENKINS_URL" "$JOB_FULL_NAME" >> "$LOG_FILE" 2>&1
+	index_html_add_entry "$JOB_FULL_NAME" "${job}"
 done
 
 index_html_trailing
