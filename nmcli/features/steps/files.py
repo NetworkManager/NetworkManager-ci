8794804df85f5e444e876935a9b66f340f90ec9b
Vladimír Beneš <vbenes@redhat.com>
2019-11-20 18:49:36 +0100

    noted_value: fix typo context.context

--- nmcli/features/steps/files.py
+++ nmcli/features/steps/files.py
@@ -99,7 +99,7 @@ def create_symlink(context, source, destination):
 @step(u'Check ifcfg-name file created with noted connection name')
 def check_ifcfg_exists(context):
     command = 'cat /etc/sysconfig/network-scripts/ifcfg-%s' % context.noted['noted-value']
-    pattern = 'NAME=%s' % context.context['noted-value']
+    pattern = 'NAME=%s' % context.noted['noted-value']
     return check_pattern_command(context, command, pattern, seconds=2)
 
 
