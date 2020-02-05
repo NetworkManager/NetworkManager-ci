2cf3e1fdf81294fa0232695680b32948ef65872b
Vladimír Beneš <vbenes@redhat.com>
2020-02-05 23:07:02 +0100

    nmstate: use again the latest stable (in our case v0.2.3 now)

--- prepare/nmstate.sh
+++ prepare/nmstate.sh
@@ -4,7 +4,7 @@ yum -y install python3-devel rpm-build
 rm -rf /usr/bin/python && ln -s /usr/bin/python3 /usr/bin/python
 git clone https://github.com/nmstate/nmstate
 cd nmstate
-#git checkout $(git tag |tail -1)
+git checkout $(git tag |tail -1)
 LC_TIME=en_US-UTF-8 sh packaging/make_rpm.sh; rc=$?
 if test $rc -eq 0; then
     rm -rf nmstate-*.src.rpm
