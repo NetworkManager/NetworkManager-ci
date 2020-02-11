3097152c4870b1f1cfbb1d9353adabb56aedd81d
Vladimír Beneš <vbenes@redhat.com>
2020-02-11 12:55:29 +0100

    env: remove mac address policy file

--- prepare/envsetup.sh
+++ prepare/envsetup.sh
@@ -128,9 +128,6 @@ install_fedora_packages () {
 
     install_plugins_dnf
 
-    # Make device mac address policy behave like old one
-    echo -e "[Match]\nOriginalName=*\n[Link]\nMACAddressPolicy=none" > /etc/systemd/network/00-NM.link
-
     # disable dhcpd dispatcher script: rhbz1758476
     [ -f /etc/NetworkManager/dispatcher.d/12-dhcpd ] && sudo chmod -x /etc/NetworkManager/dispatcher.d/12-dhcpd
 }
