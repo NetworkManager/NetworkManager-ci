e575eecbec1f7ecce2beb431e04fde28a9e51595
Vladimír Beneš <vbenes@redhat.com>
2020-03-04 00:13:40 +0100

    cico: use proper versioning

--- run/centos-ci/scripts/archive.sh
+++ run/centos-ci/scripts/archive.sh
@@ -8,17 +8,27 @@ RESULTS="/tmp/results/Test_results-*"
 KEY=$1
 
 if grep -q CentOS /etc/redhat-release ; then
-    if grep -q 7.3 /etc/redhat-release; then
-        DISTRO="centos-7-3"
-    elif grep -q 7.4 /etc/redhat-release; then
-        DISTRO="centos-7-4"
-    elif grep -q 7.5 /etc/redhat-release; then
-        DISTRO="centos-7-5"
-    elif grep -q 7.6 /etc/redhat-release; then
-        DISTRO="centos-7-6"
+    if grep -q 8.1 /etc/redhat-release; then
+        DISTRO="centos-8-1"
+    elif grep -q 8.2 /etc/redhat-release; then
+        DISTRO="centos-8-2"
+    elif grep -q 8.3 /etc/redhat-release; then
+        DISTRO="centos-8-3"
+    elif grep -q 8.4 /etc/redhat-release; then
+        DISTRO="centos-8-4"
+    elif grep -q 8.5 /etc/redhat-release; then
+        DISTRO="centos-8-5"
+    elif grep -q 8.6 /etc/redhat-release; then
+        DISTRO="centos-8-6"
+    elif grep -q 8.7 /etc/redhat-release; then
+        DISTRO="centos-8-7"
+    elif grep -q 8.8 /etc/redhat-release; then
+        DISTRO="centos-8-8"
+    elif grep -q 8.9 /etc/redhat-release; then
+        DISTRO="centos-8-9"
     fi
 
-    VERSION=$(rpm -q NetworkManager | awk -F '.el7' '{print $1}')
+    VERSION=$(rpm -q NetworkManager | awk -F '.el8' '{print $1}')
 fi
 
 
