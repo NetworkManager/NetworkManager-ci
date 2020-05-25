8d12dcc290aa54b2ecabfdb5b76f93d127277287
Vladimír Beneš <vbenes@redhat.com>
2020-05-25 10:25:07 +0200

    nmstate: use 0.3 HEAD instead of 0.3.0

--- prepare/nmstate.sh
+++ prepare/nmstate.sh
@@ -9,8 +9,8 @@ rm -rf nmstate
 git clone https://github.com/nmstate/nmstate
 cd nmstate
 # We have some regressions now so let's use 0.3's HEAD
-# git checkout nmstate-0.3
-git checkout $(git tag |tail -1)
+git checkout nmstate-0.3
+# git checkout $(git tag |tail -1)
 LC_TIME=en_US-UTF-8 sh packaging/make_rpm.sh; rc=$?
 if test $rc -eq 0; then
     rm -rf nmstate-*.src.rpm
