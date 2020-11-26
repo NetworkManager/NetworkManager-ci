aa762d5a491424f26e7dc89465c688443d8e49f7
Thomas Haller <thaller@redhat.com>
2020-11-26 18:42:02 +0100

    steps: unify "nm{cli,tui}/features/steps/steps.py"
    We should not have duplicated code, especially if it's rather simple
    helper code like this. Instead, we should unify our utilities.
    
    Make both variants identical, by adjusting them towards a common
    version.

--- nmcli/features/steps/steps.py
+++ nmcli/features/steps/steps.py
@@ -1,15 +1,16 @@
 # -*- coding: UTF-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
-from behave import step
-from time import sleep, time
-import pexpect
 import os
+import pyte
+import pexpect
 import re
 import subprocess
 from subprocess import Popen, check_output, call
+from behave import step
+from time import sleep, time
 from glob import glob
 
-# Helpers for the steps that leave the execution trace
+IS_NMTUI = "nmtui" in __file__
 
 
 def run(context, command, *a, **kw):
@@ -23,16 +24,30 @@ def run(context, command, *a, **kw):
         output = e.output.decode("utf-8", "ignore")
         returncode = e.returncode
         exception = e
-    if context is not None:
-        data = "%s\nreturncode: %d\noutput:\n%s" % (command, returncode, output)
-        context.embed("text/plain", data, caption=command[0:32] + "...")
+    if not IS_NMTUI:
+        if context is not None:
+            data = "%s\nreturncode: %d\noutput:\n%s" % (command, returncode, output)
+            context.embed("text/plain", data, caption=command[0:32] + "...")
     return output, returncode, exception
 
 
 def command_output(context, command, *a, **kw):
-    output, code, e = run(context, command, *a, **kw)
-    if code != 0:
-        raise e
+    if IS_NMTUI:
+        assert not a
+        assert not kw
+        if not os.path.isfile("/tmp/tui-screen.log"):
+            return
+        fd = open("/tmp/tui-screen.log", "a+")
+        fd.write("----------\nInfo: next failed step's '%s' output:\n" % command)
+        fd.flush()
+        output, _, _ = run(context, command)
+        fd.write(output + "\n")
+        fd.flush()
+        fd.close()
+    else:
+        output, code, e = run(context, command, *a, **kw)
+        if code != 0:
+            raise e
     return output
 
 
@@ -41,6 +56,6 @@ def command_code(context, command, *a, **kw):
     return code
 
 
-# may be usefull in another project (nmtui)
 def additional_sleep(time):
-    pass
+    if IS_NMTUI:
+        sleep(time)
--- nmtui/features/steps/steps.py
+++ nmtui/features/steps/steps.py
@@ -5,9 +5,12 @@ import pyte
 import pexpect
 import re
 import subprocess
+from subprocess import Popen, check_output, call
 from behave import step
-from time import sleep
-from subprocess import check_output
+from time import sleep, time
+from glob import glob
+
+IS_NMTUI = "nmtui" in __file__
 
 
 def run(context, command, *a, **kw):
@@ -18,22 +21,33 @@ def run(context, command, *a, **kw):
         returncode = 0
         exception = None
     except subprocess.CalledProcessError as e:
-        output = str(e.output)
+        output = e.output.decode("utf-8", "ignore")
         returncode = e.returncode
         exception = e
+    if not IS_NMTUI:
+        if context is not None:
+            data = "%s\nreturncode: %d\noutput:\n%s" % (command, returncode, output)
+            context.embed("text/plain", data, caption=command[0:32] + "...")
     return output, returncode, exception
 
 
-def command_output(context, command):
-    if not os.path.isfile("/tmp/tui-screen.log"):
-        return
-    fd = open("/tmp/tui-screen.log", "a+")
-    fd.write("----------\nInfo: next failed step's '%s' output:\n" % command)
-    fd.flush()
-    output, _, _ = run(context, command)
-    fd.write(output + "\n")
-    fd.flush()
-    fd.close()
+def command_output(context, command, *a, **kw):
+    if IS_NMTUI:
+        assert not a
+        assert not kw
+        if not os.path.isfile("/tmp/tui-screen.log"):
+            return
+        fd = open("/tmp/tui-screen.log", "a+")
+        fd.write("----------\nInfo: next failed step's '%s' output:\n" % command)
+        fd.flush()
+        output, _, _ = run(context, command)
+        fd.write(output + "\n")
+        fd.flush()
+        fd.close()
+    else:
+        output, code, e = run(context, command, *a, **kw)
+        if code != 0:
+            raise e
     return output
 
 
@@ -42,6 +56,6 @@ def command_code(context, command, *a, **kw):
     return code
 
 
-# will sleep, because in nmtui
 def additional_sleep(time):
-    sleep(time)
+    if IS_NMTUI:
+        sleep(time)
