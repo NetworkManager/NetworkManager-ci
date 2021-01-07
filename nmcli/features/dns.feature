NetworkManager/NetworkManager-ci

commit 952c16d40d6f1454070f51578dd18151f892537f

nmcli/dns: update tests for default route DNS domain with systemd-resolved
NetworkManager in 1.26.5+, 1.27.92+, 1.28.0+ and 1.29.2+ changed behavior to
no longer add a default "~." search domain for regulard devices. See
[1].

Adjust the tests for that (by adding new variants).

[1] https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/4f19fd6ce2589d749e34521a48f724e74f7307ec

nmcli/features/dns.feature
@@ -16,6 +16,10 @@ Feature: nmcli - dns
 
     @rhbz1512966
     @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
     @con_dns_remove @dns_systemd_resolved @rhelver+=8
     @dns_resolved_two_default
     Scenario: NM - dns - two connections with default route
@@ -34,12 +38,46 @@ Feature: nmcli - dns
 
     # Check eth2 configuration
     Then device "eth2" has DNS server "172.16.1.53"
-    Then device "eth2" has DNS domain "." for "domain-routing"
+    Then device "eth3" has "routing" DNS default-route
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
 
     # Check eth3 configuration
     Then device "eth3" has DNS server "172.17.1.53"
-    Then device "eth3" has DNS domain "." for "domain-routing"
+    Then device "eth3" has "routing" DNS default-route
+    Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
+
+    Then "nameserver 127.0.0.53" is visible with command "cat /etc/resolv\.conf" in "5" seconds
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @con_dns_remove @dns_systemd_resolved @rhelver+=8
+    @dns_resolved_two_default
+    Scenario: NM - dns - two connections with default route
+
+    # Create connection on eth2 with default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24 ipv4.gateway 172.16.1.2"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Bring "up" connection "con_dns"
+
+    # Create connection on eth3 with default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns2 ifname eth3 autoconnect no"
+    * Execute "nmcli connection modify con_dns2 ipv4.method manual ipv4.addresses 172.17.1.1/24 ipv4.gateway 172.17.1.2"
+    * Execute "nmcli connection modify con_dns2 ipv4.dns 172.17.1.53 ipv4.dns-search con_dns2.domain"
+    * Bring "up" connection "con_dns2"
+
+    # Check eth2 configuration
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth3" has "default" DNS default-route
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+
+    # Check eth3 configuration
+    Then device "eth3" has DNS server "172.17.1.53"
+    Then device "eth3" has "default" DNS default-route
     Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
 
     Then "nameserver 127.0.0.53" is visible with command "cat /etc/resolv\.conf" in "5" seconds
@@ -47,6 +85,10 @@ Feature: nmcli - dns
 
     @rhbz1512966
     @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
     @con_dns_remove @dns_systemd_resolved @rhelver+=8
     @dns_resolved_one_default
     Scenario: NM - dns - two connections, one with default route
@@ -65,17 +107,53 @@ Feature: nmcli - dns
 
     # Check eth2 configuration
     Then device "eth2" has DNS server "172.16.1.53"
-    Then device "eth2" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
 
     # eth3 doesn't have a default route and so doesn't get the "." domain
     Then device "eth3" has DNS server "172.17.1.53"
-    Then device "eth3" does not have DNS domain "."
+    Then device "eth2" has "no" DNS default-route
+    Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @con_dns_remove @dns_systemd_resolved @rhelver+=8
+    @dns_resolved_one_default
+    Scenario: NM - dns - two connections, one with default route
+
+    # Create connection on eth2 with default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24 ipv4.gateway 172.16.1.2"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Bring "up" connection "con_dns"
+
+    # Create connection on eth3 without default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns2 ifname eth3 autoconnect no"
+    * Execute "nmcli connection modify con_dns2 ipv4.method manual ipv4.addresses 172.17.1.1/24"
+    * Execute "nmcli connection modify con_dns2 ipv4.dns 172.17.1.53 ipv4.dns-search con_dns2.domain"
+    * Bring "up" connection "con_dns2"
+
+    # Check eth2 configuration
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth2" has "default" DNS default-route
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+
+    # eth3 doesn't have a default route and so doesn't get the "." domain
+    Then device "eth3" has DNS server "172.17.1.53"
+    Then device "eth2" has "no" DNS default-route
     Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
 
 
     @rhbz1512966
     @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
     @con_dns_remove @dns_systemd_resolved @rhelver+=8
     @dns_resolved_two_default_with_priority
     Scenario: NM - dns - two connections with default route, one has higher priority
@@ -95,12 +173,45 @@ Feature: nmcli - dns
 
     # Check eth2 configuration
     Then device "eth2" has DNS server "172.16.1.53"
-    Then device "eth2" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
 
     # eth3 doesn't have the "." domain because eth2 has higher priority
     Then device "eth3" has DNS server "172.17.1.53"
-    Then device "eth3" does not have DNS domain "."
+    Then device "eth2" has "no" DNS default-route
+    Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @con_dns_remove @dns_systemd_resolved @rhelver+=8
+    @dns_resolved_two_default_with_priority
+    Scenario: NM - dns - two connections with default route, one has higher priority
+
+    # Create connection on eth2 with default route and higher priority
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24 ipv4.gateway 172.16.1.2"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Execute "nmcli connection modify con_dns ipv4.dns-priority 10"
+    * Bring "up" connection "con_dns"
+
+    # Create connection on eth3 with default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns2 ifname eth3 autoconnect no"
+    * Execute "nmcli connection modify con_dns2 ipv4.method manual ipv4.addresses 172.17.1.1/24 ipv4.gateway 172.17.1.2"
+    * Execute "nmcli connection modify con_dns2 ipv4.dns 172.17.1.53 ipv4.dns-search con_dns2.domain"
+    * Bring "up" connection "con_dns2"
+
+    # Check eth2 configuration
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth2" has "default" DNS default-route
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+
+    # eth3 doesn't have the "." domain because eth2 has higher priority
+    Then device "eth3" has DNS server "172.17.1.53"
+    Then device "eth2" has "no" DNS default-route
     Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
 
 
@@ -135,7 +246,12 @@ Feature: nmcli - dns
 
 
     @rhbz1512966
-    @ver+=1.11.3 @rhelver+=8 @fedoraver+=31
+    @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
+    @rhelver+=8 @fedoraver+=31
     @con_dns_remove @eth0 @dns_systemd_resolved
     @dns_resolved_no_default
     Scenario: NM - dns - two connections without default route
@@ -154,17 +270,54 @@ Feature: nmcli - dns
 
     # Since there is no default route eth2 gets the "." domain
     Then device "eth2" has DNS server "172.16.1.53"
-    Then device "eth2" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
 
     # Since there is no default route eth3 gets the "." domain
     Then device "eth3" has DNS server "172.17.1.53"
-    Then device "eth3" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
+    Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @rhelver+=8 @fedoraver+=31
+    @con_dns_remove @eth0 @dns_systemd_resolved
+    @dns_resolved_no_default
+    Scenario: NM - dns - two connections without default route
+
+    # Create connection on eth2 without default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Bring "up" connection "con_dns"
+
+    # Create connection on eth3 without default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns2 ifname eth3 autoconnect no"
+    * Execute "nmcli connection modify con_dns2 ipv4.method manual ipv4.addresses 172.17.1.1/24"
+    * Execute "nmcli connection modify con_dns2 ipv4.dns 172.17.1.53 ipv4.dns-search con_dns2.domain"
+    * Bring "up" connection "con_dns2"
+
+    # Since there is no default route eth2 gets the "." domain
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth2" has "default" DNS default-route
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+
+    # Since there is no default route eth3 gets the "." domain
+    Then device "eth3" has DNS server "172.17.1.53"
+    Then device "eth3" has "default" DNS default-route
     Then device "eth3" has DNS domain "con_dns2.domain" for "domain-search"
 
 
     @rhbz1512966
     @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
     @con_dns_remove @openvpn @openvpn4 @dns_systemd_resolved @rhelver+=8
     @dns_resolved_full_tunnel_vpn
     Scenario: NM - dns - full-tunnel VPN
@@ -182,17 +335,52 @@ Feature: nmcli - dns
 
     # Check tun1 configuration
     Then device "tun1" has DNS server "172.31.70.53"
-    Then device "tun1" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
     Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
 
     # Check eth2 configuration
     Then device "eth2" has DNS server "172.16.1.53"
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
-    Then device "eth2" does not have DNS domain "."
+    Then device "eth2" has "no" DNS default-route
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @con_dns_remove @openvpn @openvpn4 @dns_systemd_resolved @rhelver+=8
+    @dns_resolved_full_tunnel_vpn
+    Scenario: NM - dns - full-tunnel VPN
+
+    # Create ethernet connection
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24 ipv4.gateway 172.16.1.2"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Bring "up" connection "con_dns"
+
+    # Create full-tunnel VPN connection
+    * Add a connection named "openvpn" for device "\*" to "openvpn" VPN
+    * Use certificate "sample-keys/client.crt" with key "sample-keys/client.key" and authority "sample-keys/ca.crt" for gateway "127.0.0.1" on OpenVPN connection "openvpn"
+    * Bring "up" connection "openvpn"
+
+    # Check tun1 configuration
+    Then device "tun1" has DNS server "172.31.70.53"
+    Then device "eth2" has "default" DNS default-route
+    Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
+
+    # Check eth2 configuration
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+    Then device "eth2" has "no" DNS default-route
 
 
     @rhbz1512966
     @ver+=1.11.3
+    @ver-1.26.5
+    @ver-1.27.91
+    @ver-1.28.0
+    @ver-1.29.2
     @con_dns_remove @openvpn @openvpn4 @dns_systemd_resolved @rhelver+=8
     @dns_resolved_split_tunnel_vpn
     Scenario: NM - dns - split-tunnel VPN
@@ -211,12 +399,44 @@ Feature: nmcli - dns
 
     # Check eth2 configuration
     Then device "eth2" has DNS server "172.16.1.53"
-    Then device "eth2" has DNS domain "." for "domain-routing"
+    Then device "eth2" has "routing" DNS default-route
     Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
 
     # Check tun1 configuration
     Then device "tun1" has DNS server "172.31.70.53"
-    Then device "tun1" does not have DNS domain "."
+    Then device "eth2" has "no" DNS default-route
+    Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
+
+
+    @rhbz1512966
+    @ver+=1.26.5
+    @ver+=1.27.91
+    @ver+=1.28.0
+    @ver+=1.29.2
+    @con_dns_remove @openvpn @openvpn4 @dns_systemd_resolved @rhelver+=8
+    @dns_resolved_split_tunnel_vpn
+    Scenario: NM - dns - split-tunnel VPN
+
+    # Create ethernet connection with default route
+    * Add a new connection of type "ethernet" and options "con-name con_dns ifname eth2 autoconnect no"
+    * Execute "nmcli connection modify con_dns ipv4.method manual ipv4.addresses 172.16.1.1/24 ipv4.gateway 172.16.1.2"
+    * Execute "nmcli connection modify con_dns ipv4.dns 172.16.1.53 ipv4.dns-search con_dns.domain"
+    * Bring "up" connection "con_dns"
+
+    # Create split-tunnel VPN connection
+    * Add a connection named "openvpn" for device "\*" to "openvpn" VPN
+    * Use certificate "sample-keys/client.crt" with key "sample-keys/client.key" and authority "sample-keys/ca.crt" for gateway "127.0.0.1" on OpenVPN connection "openvpn"
+    * Execute "nmcli con modify openvpn ipv4.never-default yes"
+    * Bring "up" connection "openvpn"
+
+    # Check eth2 configuration
+    Then device "eth2" has DNS server "172.16.1.53"
+    Then device "eth2" has "default" DNS default-route
+    Then device "eth2" has DNS domain "con_dns.domain" for "domain-search"
+
+    # Check tun1 configuration
+    Then device "tun1" has DNS server "172.31.70.53"
+    Then device "eth2" has "no" DNS default-route
     Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
 
 

nmcli/features/steps/device.py
@@ -46,6 +46,18 @@ def dns_check(dns_plugin, device, kind, arg, has):
             xhas = any((arg == d[0] for d in info['domains']))
             if has == xhas:
                 return
+        elif kind == 'default-route':
+            if arg == 'no':
+                if info['default_route'] in [None, False] and not any((d[0] == '.' for d in info['domains'])):
+                   return
+            elif arg == 'default':
+                if info['default_route'] in [None, True] and not any((d[0] == '.' for d in info['domains'])):
+                   return
+            elif arg in ['routing', 'search']:
+                if info['default_route'] in [None, True] and any((d[0] == '.' and d[1] == arg for d in info['domains'])):
+                   return
+            else:
+                raise ValueError(f"unsupported default-route kind {arg}")
         else:
             raise ValueError(f"unsupported kind \"{kind}\"")
     except Exception as e:
@@ -75,6 +87,12 @@ def dns_check_domain_has(context, device, domain, kind="domain-routing"):
 @step(u'device "{device}" does not have DNS domain "{domain}" for "{kind}"')
 def dns_check_domain_not(context, device, domain, kind='domain'):
     dns_check(context.dns_plugin, device, kind, domain, False)
+
+
+@step(u'device "{device}" has "{what}" DNS default-route')
+def dns_check_default_route_has(context, device, what):
+    assert what in ['no', 'default', 'routing', 'search']
+    dns_check(context.dns_plugin, device, 'default-route', what, None)
 
 
 @step(u'Create device "{dev}" in "{ns}" with address "{addr}"')

commit 1daf083427381768180de1ff3356fc6cf1f088fd

fix broken test @dns_resolved_full_tunnel_vpn
The last update to these tests introduced a bug
due to a copy-paste error.

Fixes: 952c16d40d6f ('nmcli/dns: update tests for default route DNS domain with systemd-resolved')

nmcli/features/dns.feature
@@ -334,7 +334,7 @@ Feature: nmcli - dns
 
     # Check tun1 configuration
     Then device "tun1" has DNS server "172.31.70.53"
-    Then device "eth2" has "routing" DNS default-route
+    Then device "tun1" has "routing" DNS default-route
     Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
 
     # Check eth2 configuration
@@ -365,7 +365,7 @@ Feature: nmcli - dns
 
     # Check tun1 configuration
     Then device "tun1" has DNS server "172.31.70.53"
-    Then device "eth2" has "default" DNS default-route
+    Then device "tun1" has "default" DNS default-route
     Then device "tun1" has DNS domain "vpn.domain" for "domain-search"
 
     # Check eth2 configuration