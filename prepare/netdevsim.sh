ff6919bf80959200262b33aa96315eabb9306635
Filip Pokryvka <fpokryvk@redhat.com>
2021-03-14 19:31:56 +0100

    prepare/netdevsim: physical addres patch + newer kernel support
    Make netdevsim devices report random MAC address created during 
    netdevsim setup as its physical address (benefitial to GUI applications, 
    as it does not report NULL as MAC address).
    
    Newer kernels does not need ethtools patch anymore, do just physical 
    address patch.
    
    Add setup argument: number of devices to create.

--- prepare/netdevsim.sh
+++ prepare/netdevsim.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 function setup () {
+    # If $1 is empty, $NUM defaults to "3"
+    NUM="${1:-3}"
     MAJOR="$(uname -r |awk -F '-' '{print $1}')"
     MINOR="$(uname -r |awk -F '-' '{print $2}'| rev| cut -d. -f2-  |rev)"
+    MINOR_NUM="$(uname -r |awk -F '-' '{print $2}'|  cut -d. -f1 )"
     LINUX=linux-$MAJOR-$MINOR
-    # We need this patched netdevsim device to support ring/coal ethtool options
+    # We need this patched netdevsim device to support ring/coal ethtool options and physical address
     PATCH="0001-netdevsim-add-mock-support-for-coalescing-and-ring-o-1.patch"
+    # for newer kernels, only physical address patch must be applied
+    # PATCH="0001-netdevsim-physical-address.patch"
     DRIVER="drivers/net/netdevsim"
 
-    if ! grep Fedora /etc/redhat-release; then
-        URL="http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/kernel"
-    else
+    if grep Fedora /etc/redhat-release; then
         URL='https://kojipkgs.fedoraproject.org//packages/kernel'
         LINUX="$(echo $LINUX |awk -F '.' '{print $1 "." $2}')"
+        PATCH="0001-netdevsim-physical-address.patch"
+    elif grep "release 8" /etc/redhat-release; then
+        URL="http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/kernel"
+        (( $MINOR_NUM >= 291 )) && PATCH="0001-netdevsim-add-mock-support-for-coalescing-and-ring-o-2.patch"
+    elif grep "release 9" /etc/redhat-release; then
+        URL="http://download.eng.bos.redhat.com/brewroot/vol/rhel-9/packages/kernel"
+        LINUX=linux-$MAJOR-$MINOR_NUM
+        PATCH="0001-netdevsim-physical-address.patch"
     fi
 
     # If we have all necessary things done
@@ -23,7 +34,8 @@ function setup () {
                wget git kernel-headers kernel-devel gcc \
                patch elfutils-libelf-devel bc yasm
 
-        # Install srpm
+        # Install srpm (first try manualy cached file in /root)
+        rpm -i /root/kernel-$MAJOR-$MINOR.src.rpm || \
         rpm -i $URL/$MAJOR/$MINOR/src/kernel-$MAJOR-$MINOR.src.rpm
         tar xf /root/rpmbuild/SOURCES/$LINUX.tar.xz -C /tmp
         cp tmp/$PATCH /tmp/$LINUX
@@ -54,7 +66,7 @@ function setup () {
     echo "** installing the patched one"
     if insmod netdevsim.ko; then
         sleep 0.5
-        echo "0 3" > /sys/bus/netdevsim/new_device
+        echo "0 $NUM" > /sys/bus/netdevsim/new_device
         touch /tmp/netdevsim
     else
         echo "** we fail to load - > exit 1"
@@ -68,8 +80,9 @@ function teardown () {
     modprobe -r netdevsim
     rm -rf /tmp/netdevsim
 }
+
 if [ "x$1" != "xteardown" ]; then
-    setup
+    setup $2
 else
     teardown
 fi
