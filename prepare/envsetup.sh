NetworkManager/NetworkManager-ci

commit a79b81f05d4847730f366e2694a6b5b4b0bb60c4

env: rhel8: install non crashing libndp 1933041

prepare/envsetup.sh
@@ -370,6 +370,9 @@ install_el8_packages () {
     # Install non crashing teamd 1684389
     dnf -y -4 update http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/libteam-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/libteam-devel-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/teamd-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/teamd-devel-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/python3-libteam-1.31-2.el8.$(arch).rpm
 
+    # Install non crashing libndp, can be removed once we are on 8.4 with automation
+    dnf -y install http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libndp/1.7/5.el8/$(arch)/libndp-1.7-5.el8.$(arch).rpm
+
     # dracut testing
     dnf -4 -y install qemu-kvm lvm2 mdadm cryptsetup iscsi-initiator-utils nfs-utils radvd gdb dhcp-client
     if [[ $(uname -p) = "s390x" ]]; then

commit 02a50d185c416fc7264036e7f799064ee2823232

fix installing libndp 1.7.5 devel package for tests
During testing, we can also build NetworkManager form source.
We thus need the libndp-devel package as well.

Otherwise, we still end up testing with libndp 1.7.3, which results
in a crash (rh #1937721).

Fixes: a79b81f05d48 ('env: rhel8: install non crashing libndp 1933041')

prepare/envsetup.sh
@@ -371,7 +371,9 @@ install_el8_packages () {
     dnf -y -4 update http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/libteam-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/libteam-devel-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/teamd-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/teamd-devel-1.31-2.el8.$(arch).rpm http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libteam/1.31/2.el8/$(arch)/python3-libteam-1.31-2.el8.$(arch).rpm
 
     # Install non crashing libndp, can be removed once we are on 8.4 with automation
-    dnf -y install http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libndp/1.7/5.el8/$(arch)/libndp-1.7-5.el8.$(arch).rpm
+    dnf -y install \
+       "http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libndp/1.7/5.el8/$(arch)/libndp-1.7-5.el8.$(arch).rpm" \
+       "http://download.eng.bos.redhat.com/brewroot/vol/rhel-8/packages/libndp/1.7/5.el8/$(arch)/libndp-devel-1.7-5.el8.$(arch).rpm"
 
     # dracut testing
     dnf -4 -y install qemu-kvm lvm2 mdadm cryptsetup iscsi-initiator-utils nfs-utils radvd gdb dhcp-client