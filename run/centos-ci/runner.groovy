f68ec788ab4d17b890f6096ee3dc3f8cb4fed2d4
Vladimír Beneš <vbenes@redhat.com>
2021-05-25 11:07:50 +0200

    cico: OMG: jenkins Groovy != Groovy
    File.exists() is fileExists() instead.. wow, just wow!

--- run/centos-ci/runner.groovy
+++ run/centos-ci/runner.groovy
@@ -55,11 +55,11 @@ node('cico-workspace') {
                 sh "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${node_hostname}:/tmp/results/* ."
                 // Check if we have RESULT so whole pipeline was not canceled
                 sh 'sleep 10'
-                if (!new File('RESULT.txt').exists()) {
+                if (!fileExists('RESULT.txt')) {
                     println("I assume we have been ABORTED as there is no RESULT.TXT")
                     sh "ls"
                     // Compilation failed there is config.log
-                    if (!new File('config.log').exists()) {
+                    if (!fileExists('config.log')) {
                         println("Pipeline canceled!")
                         cancel = "cd NetworkManager-ci; python3 run/centos-ci/pipeline_cancel.py ${env.BUILD_URL} ${GL_TOKEN} ${TD}"
                         sh "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${node_hostname} '${cancel}'"
