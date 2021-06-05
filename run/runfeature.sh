13ad4149766e7df6ecbbf4fd222e4a0cfb89eb36
Filip Pokryvka <fpokryvk@redhat.com>
2021-06-05 09:03:27 +0200

    runfeature.sh: count the tests

--- run/runfeature.sh
+++ run/runfeature.sh
@@ -3,7 +3,7 @@
 prepare/envsetup.sh first_test_setup
 
 echo "The following tests are going to be executed:"
-python -m nmci mapper_feature $1 name
+python -m nmci mapper_feature $1 name | nl --number-width=4 --starting-line-number=0 --number-format=rz --number-separator=" "
 
 set -x
 eval "$(python -m nmci mapper_feature $1 bash)"
