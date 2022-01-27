78c91a661e142ecb9c89d7c6401f7b53d28195aa
Filip Pokryvka <fpokryvk@redhat.com>
2022-01-27 09:58:29 +0100

    dracut: disable dracut legacy test on el9

--- contrib/dracut/setup.sh
+++ contrib/dracut/setup.sh
@@ -377,12 +377,14 @@ EOF
          -f $TESTDIR/initramfs.client.NM $KVERSION || exit 1
 
   # Make NFS client's dracut image using legacy network module
-  dracut -i $TESTDIR/overlay-client / \
-         -o "plymouth dash dmraid network-manager" \
-         -a "debug network-legacy ifcfg" \
-         -d "8021q ipvlan macvlan bonding af_packet piix ext3 ide-gd_mod ata_piix sd_mod e1000 nfs sunrpc" \
-         --no-hostonly-cmdline -N --no-compress \
-         -f $TESTDIR/initramfs.client.legacy $KVERSION || exit 1
+  if grep -q 'release 8' /etc/redhat-release; then
+      dracut -i $TESTDIR/overlay-client / \
+             -o "plymouth dash dmraid network-manager" \
+             -a "debug network-legacy ifcfg" \
+             -d "8021q ipvlan macvlan bonding af_packet piix ext3 ide-gd_mod ata_piix sd_mod e1000 nfs sunrpc" \
+             --no-hostonly-cmdline -N --no-compress \
+             -f $TESTDIR/initramfs.client.legacy $KVERSION || exit 1
+  fi
 
   rm -rf -- $TESTDIR/overlay-client
 
