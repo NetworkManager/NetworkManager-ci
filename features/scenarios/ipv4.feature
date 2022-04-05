NetworkManager/NetworkManager-ci

commit ecb54371b31e4dd978bd430c021fd8d7adeefc2a

ip: add test for checking order of IP addresses
The order of addresses is important. In the connection profile,
manual addresses have a defined order (see `man nm-settings`
about `ipv4.addresses` and `ipv6.addresses`). It gets more complicated,
because NetworkManager's current behavior differs depending on the
address family.

Add a test for the chosen order of IP addresses. We probably have a similar
test already. Now there is another one.

https://bugzilla.redhat.com/show_bug.cgi?id=1995372

features/scenarios/ipv4.feature
@@ -2347,3 +2347,25 @@ Feature: nmcli: ipv4
     * Bring "up" connection "con_ipv4"
     Then "activated" is visible with command "nmcli -g GENERAL.STATE con show con_ipv4" in "40" seconds
     Then "172.25.1.200" is visible with command "ip a s testX4"
+
+    @rhbz1995372
+    @ver+=1.35
+    @con_con_remove
+    @ipv4_check_addr_order
+    Scenario: nmcli - ipv4 - check IPv4 address order
+    * Add a new connection of type "ethernet" and options "con-name con_con ipv4.method auto ipv4.may-fail no ifname eth6"
+    * Bring "up" connection "con_con"
+    Then Check "ipv4" address list "/192.168.100.[0-9]+/24" on device "eth6"
+    * Execute "nmcli connection modify con_con ipv4.addresses '192.168.100.1/24,192.168.100.2/24'"
+    * Bring "up" connection "con_con"
+    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.2/24 /192.168.100.[0-9]+/24" on device "eth6"
+    * Execute "nmcli connection modify con_con ipv4.addresses '192.168.100.1/24'"
+    * Bring "up" connection "con_con"
+    Then Check "ipv4" address list "192.168.100.1/24 /192.168.100.[0-9]+/24" on device "eth6"
+    * Execute "nmcli device modify eth6 +ipv4.addresses '192.168.100.3/24'"
+    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.3/24 /192.168.100.[0-9]+/24" on device "eth6" in "3" seconds
+    * Execute "nmcli device modify eth6 ipv4.addresses ''"
+    Then Check "ipv4" address list "/192.168.100.[0-9]+/24" on device "eth6" in "3" seconds
+    * Execute "nmcli connection modify con_con ipv4.method manual ipv4.addresses '192.168.100.1/24,192.168.100.2/24,192.168.100.3/24'"
+    * Bring "up" connection "con_con"
+    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.2/24 192.168.100.3/24" on device "eth6"

features/scenarios/ipv6.feature
@@ -1608,3 +1608,26 @@
     * Execute "nmcli d reapply eth3"
     * Note the output of "ip -6 route list ::/0 dev eth3" as value "reverted"
     Then Check noted values "original" and "reverted" are the same
+
+
+    @rhbz1995372
+    @ver+=1.35
+    @con_ipv6_remove
+    @ipv6_check_addr_order
+    Scenario: nmcli - ipv6 - check IPv6 address order
+    * Add a new connection of type "ethernet" and options "ifname eth10 con-name con_ipv6 ipv4.method disabled ipv6.method auto ipv6.may-fail no ethernet.cloned-mac-address ee:aa:bb:cc:dd:ee ipv6.addr-gen-mode eui64 ipv6.ip6-privacy disabled"
+    * Bring "up" connection "con_ipv6"
+    Then Check "ipv6" address list "/2620:52:0:1086::[a-f0-9]+/128 2620:52:0:1086:ecaa:bbff:fecc:ddee/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10"
+    * Execute "nmcli connection modify con_ipv6 ipv6.addresses '1:2:3::101/64,1:2:3::102/64'"
+    * Bring "up" connection "con_ipv6"
+    Then Check "ipv6" address list "/2620:52:0:1086::[a-f0-9]+/128 2620:52:0:1086:ecaa:bbff:fecc:ddee/64 1:2:3::102/64 1:2:3::101/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10"
+    * Execute "nmcli connection modify con_ipv6 ipv6.addresses '1:2:3::101/64'"
+    * Bring "up" connection "con_ipv6"
+    Then Check "ipv6" address list "/2620:52:0:1086::[a-f0-9]+/128 2620:52:0:1086:ecaa:bbff:fecc:ddee/64 1:2:3::101/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10"
+    * Execute "nmcli device modify eth10 +ipv6.addresses '1:2:3::103/64'"
+    Then Check "ipv6" address list "/2620:52:0:1086::[a-f0-9]+/128 2620:52:0:1086:ecaa:bbff:fecc:ddee/64 1:2:3::103/64 1:2:3::101/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10" in "3" seconds
+    * Execute "nmcli device modify eth10 ipv6.addresses ''"
+    Then Check "ipv6" address list "/2620:52:0:1086::[a-f0-9]+/128 2620:52:0:1086:ecaa:bbff:fecc:ddee/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10" in "3" seconds
+    * Execute "nmcli connection modify con_ipv6 ipv6.method manual ipv6.addresses '1:2:3::101/64,1:2:3::102/64,1:2:3::103/64'"
+    * Bring "up" connection "con_ipv6"
+    Then Check "ipv6" address list "1:2:3::103/64 1:2:3::102/64 1:2:3::101/64 fe80::ecaa:bbff:fecc:ddee/64" on device "eth10" in "3" seconds

features/steps/commands.py
@@ -513,3 +513,29 @@ def check_no_coredump(context, seconds):
         if context.crash_embeded:
             assert False, "Coredump found"
         time.sleep(1)
+
+
+@step('Check "{family}" address list "{expected}" on device "{ifname}"')
+@step('Check "{family}" address list "{expected}" on device "{ifname}" in "{seconds}" seconds')
+def check_address_expect(context, family, expected, ifname, seconds=None):
+
+    expected = re.split(r"[,; ]+", expected)
+    if seconds is not None:
+        seconds = float(seconds)
+    family = nmci.ip.addr_family_norm(family)
+
+    try:
+        nmci.ip.address_expect(
+            expected=expected,
+            ifname=ifname,
+            match_mode='auto',
+            with_plen=True,
+            ignore_order=False,
+            ignore_extra=False,
+            addr_family=family,
+            wait_for_address=seconds,
+        )
+    except Exception:
+        print(">>> about to fail check_address_expect():")
+        os.system('ip -d address show')
+        raise

mapper.yaml
@@ -794,6 +794,8 @@ testmapper:
         feature: ipv6
     - ipv6_required_timeout:
         feature: ipv6
+    - ipv6_check_addr_order:
+        feature: ipv6
     - ipv6_handle_route_linking:
         feature: ipv6
     - nmcli_general_correct_profile_activated_after_restart:
@@ -1081,6 +1083,8 @@ testmapper:
     - ipv4_route-table_reapply:
         feature: ipv4
     - ipv4_required_timeout:
+        feature: ipv4
+    - ipv4_check_addr_order:
         feature: ipv4
     - dhcp_internal_ack_after_nak:
         feature: ipv4

commit 1786529ca286bcab742f3553e659a4eab62ae946

ipv4: fix expected DHCPv4 address range for @ipv4_check_addr_order
Fixes: ecb54371b31e ('ip: add test for checking order of IP addresses')

features/scenarios/ipv4.feature
@@ -2347,17 +2347,17 @@ Feature: nmcli: ipv4
     Scenario: nmcli - ipv4 - check IPv4 address order
     * Add "ethernet" connection named "con_con" for device "eth6" with options "ipv4.method auto ipv4.may-fail no"
     * Bring "up" connection "con_con"
-    Then Check "ipv4" address list "/192.168.100.[0-9]+/24" on device "eth6"
+    Then Check "ipv4" address list "/192.168.10[0-3].[0-9]+/2[24]$" on device "eth6"
     * Execute "nmcli connection modify con_con ipv4.addresses '192.168.100.1/24,192.168.100.2/24'"
     * Bring "up" connection "con_con"
-    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.2/24 /192.168.100.[0-9]+/24" on device "eth6"
+    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.2/24 /192.168.10[0-3].[0-9]+/2[24]$" on device "eth6"
     * Execute "nmcli connection modify con_con ipv4.addresses '192.168.100.1/24'"
     * Bring "up" connection "con_con"
-    Then Check "ipv4" address list "192.168.100.1/24 /192.168.100.[0-9]+/24" on device "eth6"
+    Then Check "ipv4" address list "192.168.100.1/24 /192.168.10[0-3].[0-9]+/2[24]$" on device "eth6"
     * Execute "nmcli device modify eth6 +ipv4.addresses '192.168.100.3/24'"
-    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.3/24 /192.168.100.[0-9]+/24" on device "eth6" in "3" seconds
+    Then Check "ipv4" address list "192.168.100.1/24 192.168.100.3/24 /192.168.10[0-3].[0-9]+/2[24]$" on device "eth6" in "3" seconds
     * Execute "nmcli device modify eth6 ipv4.addresses ''"
-    Then Check "ipv4" address list "/192.168.100.[0-9]+/24" on device "eth6" in "3" seconds
+    Then Check "ipv4" address list "/192.168.10[0-3].[0-9]+/2[24]$" on device "eth6" in "3" seconds
     * Execute "nmcli connection modify con_con ipv4.method manual ipv4.addresses '192.168.100.1/24,192.168.100.2/24,192.168.100.3/24'"
     * Bring "up" connection "con_con"
     Then Check "ipv4" address list "192.168.100.1/24 192.168.100.2/24 192.168.100.3/24" on device "eth6"