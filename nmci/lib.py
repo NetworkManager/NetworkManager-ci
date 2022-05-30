NetworkManager/NetworkManager-ci

commit d587ec10b190c1a4551166704a5561f1422b470e

temeouts: give various services more time to finish

nmci/lib.py
@@ -723,7 +723,10 @@ def delete_old_lock(dir, lock):
 
 def setup_libreswan(context, mode, dh_group, phase1_al="aes", phase2_al=None):
     RC = context.process.run_code(
-        f"MODE={mode} sh prepare/libreswan.sh", shell=True, ignore_stderr=True
+        f"MODE={mode} sh prepare/libreswan.sh",
+        shell=True,
+        ignore_stderr=True
+        timeout=60
     )
     if RC != 0:
         teardown_libreswan(context)

features/steps/service.py
@@ -40,7 +40,7 @@ def reboot(context):
     context.command_code("rm -rf /var/run/NetworkManager")
 
     time.sleep(1)
-    assert nmci.lib.restart_NM_service(context, reset=False), "NM restart failed"
+    assert nmci.lib.restart_NM_service(context, reset=False, timeout=10), "NM restart failed"
     time.sleep(2)
 
 
@@ -53,7 +53,7 @@ def start_NM(context):
 @step(u'Start NM without PID wait')
 def start_NM_no_pid(context):
     context.nm_restarted = True
-    assert nmci.lib.start_NM_service(context, pid_wait=False), "NM start failed"
+    assert nmci.lib.start_NM_service(context, pid_wait=False, timeout=10), "NM start failed"
 
 
 @step(u'Restart NM')

nmci/tags.py
@@ -1807,10 +1807,10 @@ def openvswitch_bs(ctx, scen):
     if ctx.process.run_code(
         "systemctl is-active openvswitch"
     ) != 0 or ctx.process.run_search_stdout(
-        "systemctl status ovs-vswitchd.service", "ERR"
+        "systemctl status ovs-vswitchd.service", "ERR", timeout=10
     ):
         print("restart openvswitch")
-        ctx.process.run_stdout("systemctl restart openvswitch")
+        ctx.process.run_stdout("systemctl restart openvswitch", timeout=10)
         nmci.lib.restart_NM_service(ctx)
 
 
@@ -2934,7 +2934,9 @@ def tag8021x_doc_procedure_bs(ctx, scen):
     shutil.copy("/etc/raddb/certs/client.key", "/etc/pki/tls/private/8021x.key")
     shutil.copy("/etc/raddb/certs/client.pem", "/etc/pki/tls/certs/8021x.pem")
     shutil.copy("/etc/raddb/certs/ca.pem", "/etc/pki/tls/certs/8021x-ca.pem")
-    ctx.process.run_stdout("yum -y install hostapd wpa_supplicant", timeout=120)
+    ctx.process.run_stdout(
+        "yum -y install hostapd wpa_supplicant", ignore_stderr=True, timeout=120
+        )
     with open("/etc/sysconfig/hostapd", "r+") as f:
         content = f.read()
         f.seek(0)

commit a0f0cbb78dfa9f4382b9ceb15808fbccc3b792c6

nmci: fix syntax error in nmci.lib.setup_libreswan()
Fixes: d587ec10b190 ('temeouts: give various services more time to finish')

nmci/lib.py
@@ -725,8 +725,8 @@ def setup_libreswan(context, mode, dh_group, phase1_al="aes", phase2_al=None):
     RC = context.process.run_code(
         f"MODE={mode} sh prepare/libreswan.sh",
         shell=True,
-        ignore_stderr=True
-        timeout=60
+        ignore_stderr=True,
+        timeout=60,
     )
     if RC != 0:
         teardown_libreswan(context)