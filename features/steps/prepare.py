NetworkManager/NetworkManager-ci

commit 4ad7a11b68bd72e3421c0e0b0e70c0f41c573d1d

avoid race in "@modify_policy_based_routing_connection"
Test "@modify_policy_based_routing_connection" activates a profile with
IPv4 and IPv6 auto methods enabled (and `ipv4.may-fail=yes`). This
means, the device might be already considered up without an IPv4
address.

Afterwards, the test calls

  * Create PBR files for profile " con_general " and " testG " device in table " 1 "

which then tries to get the IPv4 address. If it fails, because the
address is not yet configured the test will fail.

Let the "Create PBR files for profile ..." step by default retry for up
to 5 seconds to get a valid IPv4 address.

https://bugzilla.redhat.com/show_bug.cgi?id=2079409#c1

features/steps/prepare.py
@@ -1,19 +1,49 @@
+import re
+import shlex
 import time
 from behave import step
 
+import nmci.ip
+import nmci.util
 
-@step(u'Create PBR files for profile "{profile}" and "{dev}" device in table "{table}"')
-def create_policy_based_routing_files(context, profile, dev, table):
-    ips = context.command_output("nmcli connection sh %s |grep IP4.ADDRESS |awk '{print $2}'" % profile)
-    ip_slash_prefix = ips.split('\n')[0]
-    ip = ip_slash_prefix.split('/')[0]
-    gw = context.command_output("nmcli connection sh %s |grep IP4.GATEWAY |awk '{print $2}'" % profile).strip()
-    context.command_code("echo '%s dev %s table %s' > /etc/sysconfig/network-scripts/route-%s" % (ip_slash_prefix, dev, table, profile))
-    context.command_code("echo 'default via %s dev %s table %s' >> /etc/sysconfig/network-scripts/route-%s" % (gw, dev, table, profile))
 
-    context.command_code("echo 'prio 17201 iif %s table %s' > /etc/sysconfig/network-scripts/rule-%s" % (dev, table, profile))
-    context.command_code("echo 'prio 17200 from %s table %s' >> /etc/sysconfig/network-scripts/rule-%s" % (ip, table, profile))
-    time.sleep(1)
+@step('Create PBR files for profile "{profile}" and "{dev}" device in table "{table}"')
+def create_policy_based_routing_files(context, profile, dev, table, timeout=5):
+    expiry = time.monotonic()
+    if timeout:
+        expiry += timeout
+    while True:
+        s = context.process.nmcli(["connection", "sh", profile])
+        try:
+            m = re.search("^IP4\.ADDRESS\[1\]:\\s*(\\S+)\\s*$", s, re.MULTILINE)
+            ip, _, plen = nmci.ip.ipaddr_plen_norm(m.group(1), addr_family="inet")
+
+            m = re.search("^IP4\.GATEWAY:\\s*(\\S+)\\s*$", s, re.MULTILINE)
+            gw, _ = nmci.ip.ipaddr_norm(m.group(1), addr_family="inet")
+        except Exception as e:
+            if expiry >= time.monotonic():
+                raise Exception(
+                    f"Profile {profile} has no suitable IPv4 address. Output:\n\n{s})"
+                )
+            time.sleep(0.1)
+            continue
+        break
+
+    nmci.util.file_set_content(
+        f"/etc/sysconfig/network-scripts/route-{profile}",
+        [
+            f"{ip}/{plen} dev {dev} table {table}",
+            f"default via {gw} dev {dev} table {table}",
+        ],
+    )
+
+    nmci.util.file_set_content(
+        f"/etc/sysconfig/network-scripts/rule-{profile}",
+        [
+            f"prio 17201 iif {dev} table {table}",
+            f"prio 17200 from {ip} table {table}",
+        ],
+    )
 
 
 @step(u'Configure dhcp server for subnet "{subnet}" with lease time "{lease}"')

commit 7f60dd71dd873d14845efe14bef8fc8596ea989a

fix waiting in create_policy_based_routing_files()
Fixes: 4ad7a11b68bd ('avoid race in "@modify_policy_based_routing_connection"')

features/steps/prepare.py
@@ -21,7 +21,7 @@ def create_policy_based_routing_files(context, profile, dev, table, timeout=5):
             m = re.search("^IP4\.GATEWAY:\\s*(\\S+)\\s*$", s, re.MULTILINE)
             gw, _ = nmci.ip.ipaddr_norm(m.group(1), addr_family="inet")
         except Exception as e:
-            if expiry >= time.monotonic():
+            if time.monotonic() >= expiry:
                 raise Exception(
                     f"Profile {profile} has no suitable IPv4 address. Output:\n\n{s})"
                 )