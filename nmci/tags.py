NetworkManager/NetworkManager-ci

commit 6dd8df14750a716f8f0b5bb23bd75d7cf5d8da51

tags/prepare_patched_netdevsim_bs: wait for NM to notice the device
It's not sufficient to wait until the platform notices. NM needs to see
thedevice.

nmci/tags.py
@@ -1482,7 +1482,16 @@ def prepare_patched_netdevsim_bs(context, scenario):
         "sh prepare/netdevsim.sh setup", timeout=600, ignore_stderr=True
     )
     nmci.ip.link_set(ifname="eth11", up=True, wait_for_device=1)
-    nmci.ip.link_show(ifname="eth11", flags="LOWER_UP", timeout=1)
+
+    # Wait until NetworkManager notices the device
+    timeout = time.monotonic() + 10
+    while time.monotonic() < timeout:
+        if context.process.run(
+            "nmcli -f GENERAL.STATE device show eth11", "disconnected"
+        ):
+            return
+        time.sleep(0.1)
+    raise Exception("Timed out waiting for eth11 to be seen by NetworkManager")
 
 
 def prepare_patched_netdevsim_as(context, scenario):

commit 9901db6cadcc3e1d165574f793ed5633ae9c19e5

tags: fix netdevsim check condition
Fixes: 6dd8df14750a ('tags/prepare_patched_netdevsim_bs: wait for NM to notice the device')

nmci/tags.py
@@ -1486,12 +1486,15 @@ def prepare_patched_netdevsim_bs(context, scenario):
     # Wait until NetworkManager notices the device
     timeout = time.monotonic() + 10
     while time.monotonic() < timeout:
-        if context.process.run(
-            "nmcli -f GENERAL.STATE device show eth11", "disconnected"
+        if context.process.run_search_stdout(
+            "nmcli -f GENERAL.STATE device show eth11",
+            "disconnected",
+            ignore_stderr=True,
+            ignore_returncode=True,
         ):
             return
         time.sleep(0.1)
-    raise Exception("Timed out waiting for eth11 to be seen by NetworkManager")
+    assert False, "Timed out waiting for eth11 to be seen by NetworkManager"
 
 
 def prepare_patched_netdevsim_as(context, scenario):