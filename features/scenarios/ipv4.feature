NetworkManager/NetworkManager-ci

commit f16e556607c86c0cf2049a5035b47cf5ca8dff64

ipv4: adjust version of ipv4_routing_rules_manipulation

features/scenarios/ipv4.feature
@@ -2356,6 +2356,25 @@ Feature: nmcli: ipv4
 
     @rhbz1652653 @rhbz1696881
     @ver+=1.18.4
+    @ver-=1.40
+    @restart_if_needed
+    @ipv4_routing_rules_manipulation
+    Scenario: NM - ipv4 - routing rules manipulation
+    * Add "ethernet" connection named "con_ipv4" for device "eth3" with options "autoconnect no"
+    * Bring "up" connection "con_ipv4"
+    * Modify connection "con_ipv4" changing options "ipv4.routing-rules 'priority 5 table 6, priority 6 from 192.168.6.7/32 table 7' autoconnect yes"
+    * Bring "up" connection "con_ipv4"
+    When "activated" is visible with command "nmcli -g GENERAL.STATE con show con_ipv4" in "45" seconds
+    * Reboot
+    When "activated" is visible with command "nmcli -g GENERAL.STATE con show con_ipv4" in "45" seconds
+    When "5:\s+from all lookup 6\s+6:\s+from 192.168.6.7 lookup 7" is visible with command "ip rule"
+    * Bring "down" connection "con_ipv4"
+    Then "5:\s+from all lookup 6\s+6:\s+from 192.168.6.7 lookup 7" is not visible with command "ip rule"
+    And "Exactly" "3" lines are visible with command "ip rule"
+
+
+    @rhbz1652653 @rhbz1696881
+    @ver+=1.40.1
     @restart_if_needed
     @ipv4_routing_rules_manipulation
     Scenario: NM - ipv4 - routing rules manipulation

commit 45037788b4d769840057ed49baa41e2cff757bcc

ipv4: adjust version tags in @ipv4_routing_rules_manipulation test
The change was done in 1.41.4+ ([1]) and backported to 1.40.3+ ([2]).
Thus the proper tags are as such.

Also, when a test is guared by @ver+=X, then the older version
should have exactly(!) @ver-X, and not come up with different version
tags. That is, because we want that there is no overlap and no gap
between the version for which the test is enabled.

[1] https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/82009e21d28b5f3d61b9e1e3cebbf2dd94e8f32a
[2] https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/commit/4e857e33fca0f6f92fdc378593b7bd102f2a6572

Fixes: f16e556607c8 ('ipv4: adjust version of ipv4_routing_rules_manipulation')

features/scenarios/ipv4.feature
@@ -2356,7 +2356,8 @@ Feature: nmcli: ipv4
 
     @rhbz1652653 @rhbz1696881
     @ver+=1.18.4
-    @ver-=1.40
+    @ver-1.40.3
+    @ver-1.41.4
     @restart_if_needed
     @ipv4_routing_rules_manipulation
     Scenario: NM - ipv4 - routing rules manipulation
@@ -2374,7 +2375,8 @@ Feature: nmcli: ipv4
 
 
     @rhbz1652653 @rhbz1696881
-    @ver+=1.40.1
+    @ver+=1.40.3
+    @ver+=1.41.4
     @restart_if_needed
     @ipv4_routing_rules_manipulation
     Scenario: NM - ipv4 - routing rules manipulation