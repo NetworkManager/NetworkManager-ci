afb97234441f02e7667c23da4f16e7fa19e2bb68
Filip Pokryvka <fpokryvk@redhat.com>
2023-01-20 06:09:09 +0100

    fixup veth

--- nmci/veth.py
+++ nmci/veth.py
@@ -42,8 +42,8 @@ class _Veth:
     def teardown_testveth(self, ns):
         print(f"Removing the setup in {ns} namespace")
         if os.path.isfile(f"/tmp/{ns}.pid"):
-            nmci.ip.netns_exec(ns, "pkill", "-SIGCONT", "-F", f"/tmp/{ns}.pid")
-            nmci.ip.netns_exec(ns, "pkill", "-F", f"/tmp/{ns}.pid")
+            nmci.ip.netns_exec(ns, "pkill", "-SIGCONT", "-F", f"/tmp/{ns}.pid", check=False)
+            nmci.ip.netns_exec(ns, "pkill", "-F", f"/tmp/{ns}.pid", check=False)
         device = ns.split("_")[0]
         print(device)
         nmci.process.run(f"pkill -F /var/run/dhclient-{device}.pid", ignore_stderr=True)
