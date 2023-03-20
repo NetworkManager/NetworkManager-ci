c27ca97d364bd334aeb021a874c80daf9ada23b6
Filip Pokryvka <fpokryvk@redhat.com>
2023-03-20 07:52:55 +0100

    Revert "contrib.gui: preserve selinux context in restore step"
    This reverts commit 26cc1eb477c935b5339b4e8b7d8c99088425296d.
    
    We need to do this with elevated privileges, since gui tests run under
    user test.

--- contrib/gui/steps.py
+++ contrib/gui/steps.py
@@ -15,7 +15,6 @@
 from behave import step  # pylint: disable=no-name-in-module
 from qecore.step_matcher import use_step_matcher
 import subprocess
-import shutil
 import os
 import sys
 import re
@@ -433,12 +432,13 @@ def restore_device(context, device, connection):
     assert os.path.isfile(
         cfile
     ), f"unable to find configuration file for '{connection}' on {device}:\n{check_cmd_out}"
-    # copy2 preserves selinux context
-    shutil.copy2(cfile, f"/tmp/backup_{connection}")
+    assert (
+        subprocess.call(f"sudo cp '{cfile}' '/tmp/backup_{connection}'", shell=True)
+        == 0
+    ), f"unable to backup file '{cfile}'"
 
     def restore(connection, cfile):
-        # move preserves selinux context
-        shutil.move(f"/tmp/backup_{connection}", cfile)
+        subprocess.call(f"sudo mv '/tmp/backup_{connection}' '{cfile}'", shell=True)
         subprocess.call("sudo nmcli con reload", shell=True)
         assert (
             subprocess.call(f"sudo nmcli con up '{connection}'", shell=True) == 0
