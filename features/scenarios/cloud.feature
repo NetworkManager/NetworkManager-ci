e6b1d8863de59f238135892bbe87a21d78d79b9f
Lubomir Rintel <lkundrak@v3.sk>
2023-04-24 01:25:38 +0200

    cloud: add aliyun test
    A basic test that configures multiple addresses on a single interface.

--- features/scenarios/cloud.feature
+++ features/scenarios/cloud.feature
@@ -6,3 +6,21 @@ Feature: nmcli: cloud
     # @other_tags (see environment.py)
     # @test_name (compiled from scenario name)
     # Scenario:
+
+
+    @cloud
+    @ethernet
+    @cloud_aliyun_basic
+    Scenario: cloud - aliyun - Basic Aliyun nm-cloud-setup checks
+    * Bring "up" connection "testeth1"
+    * Note MAC address output for device "eth0" via ip command as "mac0"
+    * Mock Aliyun metadata for device with MAC address "mac0"
+    * Mock Aliyun CIDR block "172.31.16.0/20" for device with MAC address "mac0"
+    * Mock Aliyun IP addresses "172.31.176.249" and "172.31.17.249" with mask "255.255.240.0" for device with MAC address "mac0"
+    * Mock Aliyun Gateway "172.31.176.1" for device with MAC address "mac0"
+    * Check "ipv4" address list "/192.168.[0-9]+.[0-9]+/24$" on device "eth0"
+    * Execute "NM_CLOUD_SETUP_ALIYUN=yes NM_CLOUD_SETUP_LOG=trace /usr/libexec/nm-cloud-setup"
+    Then Check "ipv4" address list "/192.168.[0-9]+.[0-9]+/24$ 172.31.176.249/20 172.31.17.249/20" on device "eth0"
+    * Mock Aliyun IP addresses "172.31.186.249" and "172.31.18.249" with mask "255.255.240.0" for device with MAC address "mac0"
+    * Execute "NM_CLOUD_SETUP_ALIYUN=yes NM_CLOUD_SETUP_LOG=trace /usr/libexec/nm-cloud-setup"
+    Then Check "ipv4" address list "/192.168.[0-9]+.[0-9]+/24$ 172.31.186.249/20 172.31.18.249/20" on device "eth0"
--- features/steps/cloud.py
+++ features/steps/cloud.py
@@ -2,3 +2,68 @@
 from behave import step
 
 import nmci
+
+
+@step('Mock Aliyun metadata for device with MAC address "{mac}"')
+def aliyun_mac(context, mac):
+    mac_addr = context.noted[mac].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/ --data '{mac_addr}'"
+    )
+
+
+@step('Mock Aliyun metadata for devices with MAC addresses "{mac0}" and "{mac1}"')
+def aliyun_macs(context, mac0, mac1):
+    mac_addr0 = context.noted[mac0].strip()
+    mac_addr1 = context.noted[mac1].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/ --data '{mac_addr0}\n{mac_addr1}'"
+    )
+
+
+@step(
+    'Mock Aliyun IP address "{ip_addr}" with mask "{netmask}" for device with MAC address "{mac}"'
+)
+def aliyun_ip(context, ip_addr, netmask, mac):
+    mac_addr = context.noted[mac].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/private-ipv4s --data '{ip_addr}'"
+    )
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/primary-ip-address --data '{ip_addr}'"
+    )
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/netmask --data '{netmask}'"
+    )
+
+
+@step(
+    'Mock Aliyun IP addresses "{ip_addr1}" and "{ip_addr2}" with mask "{netmask}" for device with MAC address "{mac}"'
+)
+def aliyun_ip(context, ip_addr1, ip_addr2, netmask, mac):
+    mac_addr = context.noted[mac].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/private-ipv4s --data '{ip_addr1},{ip_addr2}'"
+    )
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/primary-ip-address --data '{ip_addr1}'"
+    )
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/netmask --data '{netmask}'"
+    )
+
+
+@step('Mock Aliyun CIDR block "{cidr}" for device with MAC address "{mac}"')
+def aliyun_cidr(context, cidr, mac):
+    mac_addr = context.noted[mac].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/vpc-cidr-block --data '{cidr}'"
+    )
+
+
+@step('Mock Aliyun Gateway "{gw_addr}" for device with MAC address "{mac}"')
+def aliyun_ip(context, gw_addr, mac):
+    mac_addr = context.noted[mac].strip()
+    nmci.process.run_stdout(
+        f"curl -s -X PUT http://localhost/2016-01-01/meta-data/network/interfaces/macs/{mac_addr}/gateway --data '{gw_addr}'"
+    )
