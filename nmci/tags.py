NetworkManager/NetworkManager-ci

commit fc328005827c7f5eafee81d26b47b3ee674abb97

nmci/tags: let logging_info_only use std. cleanup
We should remove the config file in cleanup.

nmci/tags.py
@@ -287,19 +287,13 @@ _register_tag("regenerate_veth", None, regenerate_veth_as)
 
 def logging_info_only_bs(context, scenario):
     conf = "/etc/NetworkManager/conf.d/99-xlogging.conf"
+    nmci.cleanup.cleanup_nm_config(conf)
     nmci.util.file_set_content(conf, ["[logging]", "level=INFO", "domains=ALL"])
     time.sleep(0.5)
     nmci.nmutil.restart_NM_service()
 
 
-def logging_info_only_as(context, scenario):
-    conf = "/etc/NetworkManager/conf.d/99-xlogging.conf"
-    context.process.run_stdout(f"rm -rf {conf}")
-    # this is after performance tests, so NM restart can take a while
-    nmci.nmutil.restart_NM_service(timeout=120)
-
-
-_register_tag("logging_info_only", logging_info_only_bs, logging_info_only_as)
+_register_tag("logging_info_only", logging_info_only_bs, None)
 
 
 def _is_container():

commit f740702da4bcf2cb34bd2932469ac181c6d3b85f

tags: fix cleanup for @logging_info_only tag
Previously, the cleanup happens with priority=nmci.cleanup.PRIORITY_NM_SERVICE_RESTART
and a timeout of 15 seconds.

That changes in commit fc328005827c ('nmci/tags: let logging_info_only
use std. cleanup'), which breaks the @vlan_create_many_vlans test (where
restarting the deamon takes longer than 15 seconds).

Fixes: fc328005827c ('nmci/tags: let logging_info_only use std. cleanup')

nmci/tags.py
@@ -287,7 +287,15 @@ _register_tag("regenerate_veth", None, regenerate_veth_as)
 
 def logging_info_only_bs(context, scenario):
     conf = "/etc/NetworkManager/conf.d/99-xlogging.conf"
-    nmci.cleanup.cleanup_nm_config(conf)
+    nmci.cleanup.cleanup_nm_config(
+        conf,
+        schedule_nm_restart=False,
+        priority=nmci.Cleanup.PRIORITY_TAG,
+    )
+    nmci.cleanup.cleanup_add_NM_service(
+        timeout=120,
+        priority=nmci.Cleanup.PRIORITY_TAG,
+    )
     nmci.util.file_set_content(conf, ["[logging]", "level=INFO", "domains=ALL"])
     time.sleep(0.5)
     nmci.nmutil.restart_NM_service()