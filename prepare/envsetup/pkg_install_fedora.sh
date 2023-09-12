8175ee5a7ae89deb95e3268593789127ad9d9903
Vladimír Beneš <vbenes@redhat.com>
2023-09-12 11:14:20 +0200

    envsetup: install podman in fedora too

--- prepare/envsetup/pkg_install_fedora.sh
+++ prepare/envsetup/pkg_install_fedora.sh
@@ -28,7 +28,7 @@ install_fedora_packages () {
         psmisc firewalld dhcp-server ethtool python3-dbus python3-gobject dnsmasq \
         tcpdump wireshark-cli iputils iproute-tc gdb gcc wireguard-tools rp-pppoe tuned \
         mptcpd wpa_supplicant NetworkManager-initscripts-ifcfg-rh s390utils-base nmstate \
-        python3-systemd bind-utils valgrind \
+        python3-systemd bind-utils podman valgrind \
         --skip-broken
 
     # freeradius
