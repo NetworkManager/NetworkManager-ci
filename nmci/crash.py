5c695f720f4fbb125506e1e7133f21625f801303
Filip Pokryvka <fpokryvk@redhat.com>
2023-10-09 21:25:46 +0200

    embed qemu and kvm coredumps

--- nmci/crash.py
+++ nmci/crash.py
@@ -20,6 +20,8 @@ def check_dump_package(pkg_name):
         pkg_name in ["NetworkManager", "ModemManager", "nmcli", "nmtui", "behave"]
         or "ovs" in pkg_name
         or "openvswitch" in pkg_name
+        or "qemu" in pkg_name
+        or "kvm" in pkg_name
     ):
         return True
     return False
