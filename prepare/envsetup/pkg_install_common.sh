f21b5f6319901b6e63aa284061d56e60780f1f98
Filip Pokryvka <fpokryvk@redhat.com>
2025-04-17 10:15:27 +0200

    envsetup: install iptables if missing in compose

--- prepare/envsetup/pkg_install_common.sh
+++ prepare/envsetup/pkg_install_common.sh
@@ -14,7 +14,7 @@ install_common_packages () {
     K_MINOR="$(uname -r |awk -F '-' '{print $2}'| rev| cut -d. -f2-  |rev)"
     PKGS_INSTALL="$PKGS_INSTALL \
         bash-completion bc bind-utils dbus-x11 dhcp-relay dhcp-server dnsmasq elfutils-libelf-devel ethtool firewalld \
-        freeradius gcc git hostapd httpd iperf3 iproute-tc iputils iw jq kernel-headers kernel-devel-$K_MAJOR-$K_MINOR \
+        freeradius gcc git hostapd httpd iperf3 iproute-tc iptables iputils iw jq kernel-headers kernel-devel-$K_MAJOR-$K_MINOR \
         libreswan-debuginfo lshw lsof mptcpd net-tools nmap-ncat nmstate openssl-pkcs11 patch podman psmisc python3-dbus \
         python3-gobject python3-inotify python3-libselinux python3-netaddr python3-pip python3-systemd \
         rsync s390utils-base tcpdump telnet tuned valgrind valgrind-gdb wget wireshark-cli wpa_supplicant yasm ipcalc"
