NetworkManager/NetworkManager-ci

commit 72251caee5b1ea8bab191db6fea6b34e016df9f1

Backport tests for 9.9
Target build to test: NetworkManager-1.54.4-3.el9

features/scenarios/general.feature
@@ -4129,6 +4129,7 @@ Feature: nmcli - general
 
     @ver+=1.57
     @ver/rhel/10/3+=1.58.0.1
+    @ver/rhel/9/9+=1.54.4.3
     @print_config_no_avc_error
     Scenario: NM - general - check that there is no avc error with print-config
     * Cleanup execute "rpm -q gvfs-client && dnf remove -y gvfs-client" with timeout "120" seconds

features/scenarios/ipv6.feature
@@ -597,6 +597,7 @@
 
     @ver+=1.57.4
     @ver/rhel/10/3+=1.58.0.1
+    @ver/rhel/9/9+=1.54.4.3
     @NMT-2218
     @eth0
     @ipv6_ignore-auto-dns_reapply

features/scenarios/ovs.feature
@@ -2187,6 +2187,7 @@ interfaces:
 
     @ver+=1.57.5
     @ver/rhel/10/3+=1.58.0.1
+    @ver/rhel/9/9+=1.54.4.3
     @tcpdump
     @openvswitch
     @ovs_dhcp_send_release_internal

commit bf530d9cdf2251c702ba5e244a49f207e622506e

Fix backports to work with el9 and el10 suffixes
RHEL packages for y stream end with el9 and el10
suffixes.

A tag like `@ver/rhel/9/9+=1.54.4.3` will actually
be used for RHEL 9.9z stream where the packages will
have el9_9 suffix.

Fixes: 72251caee5b1ea8bab191db6fea6b34e016df9f1
Fixes: f516738eefbe055edfd8424fbeacdbf40f93cb4e
Fixes: e85464cfefd05bf44519718b5f6cc98ea773a4c9
Fixes: 9cb18c88673c826cd28eac25ac7798fe7b69a7c7

features/scenarios/general.feature
@@ -4128,8 +4128,8 @@ Feature: nmcli - general
     * "probe created with ebpf enabled" is visible in journal
 
     @ver+=1.57
-    @ver/rhel/10/3+=1.58.0.1
-    @ver/rhel/9/9+=1.54.4.3
+    @ver/rhel/10+=1.58.0.1
+    @ver/rhel/9+=1.54.4.3
     @print_config_no_avc_error
     Scenario: NM - general - check that there is no avc error with print-config
     * Cleanup execute "rpm -q gvfs-client && dnf remove -y gvfs-client" with timeout "120" seconds

features/scenarios/ipv6.feature
@@ -596,8 +596,8 @@
 
 
     @ver+=1.57.4
-    @ver/rhel/10/3+=1.58.0.1
-    @ver/rhel/9/9+=1.54.4.3
+    @ver/rhel/10+=1.58.0.1
+    @ver/rhel/9+=1.54.4.3
     @NMT-2218
     @eth0
     @ipv6_ignore-auto-dns_reapply

features/scenarios/ovs.feature
@@ -2186,8 +2186,8 @@ interfaces:
 
 
     @ver+=1.57.5
-    @ver/rhel/10/3+=1.58.0.1
-    @ver/rhel/9/9+=1.54.4.3
+    @ver/rhel/10+=1.58.0.1
+    @ver/rhel/9+=1.54.4.3
     @tcpdump
     @openvswitch
     @ovs_dhcp_send_release_internal