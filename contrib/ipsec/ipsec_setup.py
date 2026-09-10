60a14ad60448d759bd3f31ae11ced7f96009610e
Vladimír Beneš <vbenes@redhat.com>
2026-09-10 10:22:24 +0200

    test: replace libreswan RPMs with routing state cleanup fix
    Replace -1.el10_3 crash-fix RPMs with -2.el10_4 RPMs that fix
    TEARDOWN_CHILD routing dispatch for INSTANCE connections. This
    fixes the server leaving stale routing state after a failed
    IKE_AUTH, which caused subsequent connections to get TS_UNACCEPTABLE.

--- contrib/ipsec/ipsec_setup.py
+++ contrib/ipsec/ipsec_setup.py
@@ -37,7 +37,7 @@ if env == "4in6_6in4":
     IpsecTestEnv.start_ipsec_srv_4in6_6in4()
 
 # Patch libreswan in the server container with fixed RPMs
-# (fixes PASSERT crash in terminate_and_down_and_unroute_connections)
+# (fixes routing state cleanup for failed IKE_AUTH on INSTANCE connections)
 CONTAINER_NAME = "nmstate-ipsec-srv"
 import platform
 arch = platform.machine()
